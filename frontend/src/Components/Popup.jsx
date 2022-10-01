b88bc725b66e446a1efc6bd43c16b3a3f13a6825
김혜진 <khj000217@naver.com>
2022-10-02 01:22:50 +0900

    feat: #3 Input Popup 컴포넌트

--- frontend/src/Components/Popup.jsx
+++ frontend/src/Components/Popup.jsx
@@ -1,5 +1,48 @@
+/* eslint-disable no-undef */
 import React from 'react';
+import styled from 'styled-components';
+import Btn from './Btn';
 
-export default function Popup() {
-  return <div>Popup</div>;
+const Wrap = styled.div`
+  width: 25rem;
+  height: wrap-content;
+  border-radius: 0.3rem;
+  border: 0.15rem solid rgba(128, 109, 70);
+  font-family: 'Noto Sans KR', sans-serif;
+  div {
+    margin: 1.2rem;
+  }
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 1.4rem;
+`;
+const Message = styled.div``;
+
+const Buttons = styled.div`
+  text-align: right;
+`;
+
+export default function Popup({ color, background, width, height }) {
+  return (
+    <Wrap>
+      <Title>제목 확인</Title>
+      <Message>메세지 확인</Message>
+      <Buttons>
+        <Btn
+          color={color}
+          background={background}
+          width={width}
+          height={height}
+        />
+        <Btn
+          color={color}
+          background={background}
+          width={width}
+          height={height}
+        />
+      </Buttons>
+    </Wrap>
+  );
 }
