c026480e2c6ecc66302c7ddf6a409d081ff1678b
Choejeonghun <saa00123@naver.com>
2022-10-01 20:42:22 +0900

    feat:#3 스크롤 컴포넌트

--- frontend/src/Components/Scroll.jsx
+++ frontend/src/Components/Scroll.jsx
@@ -1,6 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
+const Body = styled.div`
+  box-sizing: border-box;
+  border: 0.2rem solid rgba(128, 109, 70);
+  width: 10rem;
+  height: 5rem;
+`;
+
+const Scrollbar = styled.nav`
+  display: flex;
+  overflow: auto;
+  height: 4rem;
+  &::-webkit-scrollbar {
+    width: 0.5rem;
+    height: 1rem;
+    border-radius: 0.4rem;
+    background: rgba(158, 197, 124);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 0.4rem;
+  }
+`;
+
 export default function Scroll() {
-  return <div>Scroll</div>;
+  return (
+    <Body>
+      <Scrollbar>
+        <ul>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+      </Scrollbar>
+    </Body>
+  );
 }
