c4b7a3d7b5e3120e9fc32a889a2b91cd7cae8a46
Choejeonghun <saa00123@naver.com>
2022-10-01 23:15:36 +0900

    feat:#3 plusbtn

--- frontend/src/Components/PlusBtn.jsx
+++ frontend/src/Components/PlusBtn.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PlusBtn1 = styled.button`
+  font-size: 1rem;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  color: blue;
+  width: 1.5rem;
+  height: 1.5rem;
+  border: 0.1rem solid black;
+  border-radius: 100%;
+  background-color: white;
+`;
 
 export default function PlusBtn() {
-  return <div>PlusBtn</div>;
+  return <PlusBtn1>+</PlusBtn1>;
 }
