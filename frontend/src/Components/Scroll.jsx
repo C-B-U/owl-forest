e700323544e889f8360649e53d2f97829677a2de
Choejeonghun <saa00123@naver.com>
2022-10-01 19:20:37 +0900

    feat:#3 검색창 컴포넌트

--- frontend/src/Components/Scroll.jsx
+++ frontend/src/Components/Scroll.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 export default function Scroll() {
   return <div>Scroll</div>;
--- frontend/src/Components/Search.jsx
+++ frontend/src/Components/Search.jsx
@@ -1,5 +1,63 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const boxFade = keyframes`
+100%{
+  width:200px;
+  transform:translateX(-50%);
+}
+`;
+
+const inputFade = keyframes`
+0%{
+  opacity:0;
+}
+100%{
+  opacity:1;
+}
+`;
+
+const SearchBox = styled.form`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  width: 50px;
+  height: 50px;
+  background: rgba(158, 197, 124);
+  box-sizing: border-box;
+  border-radius: 25px;
+  border: 4px solid rgba(128, 109, 70);
+  padding: 5px;
+  &:hover {
+    cursor: pointer;
+    animation: ${boxFade} 1s forwards;
+  }
+`;
+
+const InputBox = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 150px;
+  height: 42.5px;
+  line-height: 30px;
+  outline: 0;
+  border: 0;
+  opacity: 0;
+  font-size: 1em;
+  border-radius: 20px;
+  padding: 0 20px;
+  &:hover {
+    display: block;
+    cursor: pointer;
+    animation: ${inputFade} 1s forwards;
+  }
+`;
 
 export default function Search() {
-  return <div>Search</div>;
+  return (
+    <SearchBox action=''>
+      <InputBox type='text' placeholder='Search' onChange />
+    </SearchBox>
+  );
 }
