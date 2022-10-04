28c14b6e09cdd12e85a2e62425b2dc556c40276d
Choejeonghun <saa00123@naver.com>
2022-10-04 20:59:36 +0900

    refactor:#36 각 서비스 라우팅 병합 및 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ActivityLog from './Pages/ActivityLog';
 import PlanMake from './Pages/PlanMake';
@@ -20,14 +19,14 @@ import BorrowList from './Pages/BR_BorrowList';
 import ReviewDetail from './Pages/BR_ReviewDetail';
 import ReviewList from './Pages/BR_ReviewList';
 import ReviewReg from './Pages/BR_ReviewReg';
-import Popup from './Components/Popup';
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Routes>
-          <Route path='/' element={<ActivityLog />} />
+          {/* ---- 활동일지 ---- */}
+          <Route path='/ActivityLog' element={<ActivityLog />} />
           <Route path='/PlanMake' element={<PlanMake />} />
           <Route path='/StudyCreation' element={<StudyCreation />} />
           <Route path='/StudyCrew' element={<StudyCrew />} />
@@ -35,20 +34,22 @@ function App() {
           <Route path='/StudyLog' element={<StudyLog />} />
           <Route path='/StudyManage' element={<StudyManage />} />
           <Route path='/Writing' element={<Writing />} />
-        </Routes>
-      </Router>
-          <Route path='/' element={<Login />} />
+          {/* ---- 회원관리 ---- */}
+          <Route path='/Login' element={<Login />} />
           <Route path='/FindId' element={<FindId />} />
           <Route path='/FindPw' element={<FindPw />} />
           <Route path='/MemList' element={<MemList />} />
           <Route path='/MemListCon' element={<MemListCon />} />
           <Route path='/Reg' element={<Reg />} />
           <Route path='/Profile' element={<Profile />} />
-          <Route path='/' element={<BorrowList />} />
+          {/* ---- 북리뷰 ---- */}
+          <Route path='/BorrowList' element={<BorrowList />} />
           <Route path='/BookRegister' element={<BookReg />} />
           <Route path='/ReviewDetail' element={<ReviewDetail />} />
           <Route path='/ReviewList' element={<ReviewList />} />
           <Route path='/ReviewRegister' element={<ReviewReg />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
