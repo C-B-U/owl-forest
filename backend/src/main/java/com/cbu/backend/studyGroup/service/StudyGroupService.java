dcdbbb55c440a671b27f909eed5f95fbc2880051
ohksj77 <ohksj77@gmail.com>
2022-10-05 23:31:36 +0900

    feat:#30 스터디 생성 및 스터디 id로 조회

--- backend/src/main/java/com/cbu/backend/studyGroup/service/StudyGroupService.java
+++ backend/src/main/java/com/cbu/backend/studyGroup/service/StudyGroupService.java
@@ -6,6 +6,7 @@ import com.cbu.backend.studyGroup.repository.StudyGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException; // Exception 수정 필요
 import java.util.Optional;
 
 @Service
@@ -13,6 +14,25 @@ import java.util.Optional;
 public class StudyGroupService {
 
     private final StudyGroupRepository studyGroupRepository;
+
+    /*
+    public Long registerStudyGroup(CreateStudyGroupRequestDTO createStudyGroupRequestDTO) {
+        StudyGroup studyGroup = createStudyGroupRequestDTO.toStudyGroup();
+        if(studyGroup.isDuplicatedMember(studyGroup)){
+            throw new RuntimeException(); // Exception 수정 필요
+        }
+        studyGroupRepository.save(studyGroup);
+        return studyGroup.getId();
+    }
+    */
+
+    public StudyGroup searchById(Long id) {
+        Optional<StudyGroup> studyGroup = studyGroupRepository.findById(id);
+        if(!studyGroup.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+        return studyGroup.get();
+    }
 
     public void updateLike(Long studyGroupId, Integer like) {
         studyGroupRepository
