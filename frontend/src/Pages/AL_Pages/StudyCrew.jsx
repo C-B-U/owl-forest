bf7c44c39715d739142ec3c5379e5e96d3098b43
suhyeon <suhyeon3484@naver.com>
2022-10-09 22:04:39 +0900

    feat : header

--- frontend/src/Pages/AL_Pages/StudyCrew.jsx
+++ frontend/src/Pages/AL_Pages/StudyCrew.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { palette } from 'styled-tools';
+import Logo10 from '../../Image/Logo10.png';
+
+const HeaderWrapper = styled.div`
+  height: 4rem;
+  display: flex;
+  background-color: ${palette('PsGreen', 0)};
+  padding: 0 5rem;
+`;
+
+const Logo = styled.div`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 100%;
+  display: flex;
+  margin-right: 1rem;
+  background-image: url(${Logo10});
+  background-size: 4.4rem;
+  background-repeat: no-repeat;
+`;
+
+const Title = styled.h1`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20pt;
+  display: flex;
+`;
 
 function StudyCrew() {
-  return <div>StudyCrew</div>;
+  return (
+    <div>
+      <HeaderWrapper>
+        <Logo />
+        <Title>활동일지 서비스</Title>
+      </HeaderWrapper>
+    </div>
+  );
 }
 
 export default StudyCrew;
