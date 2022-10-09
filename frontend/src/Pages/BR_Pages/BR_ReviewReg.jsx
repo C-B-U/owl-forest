9530631ea79808e5f7b3b723c7fc41fdb66934b7
김혜진 <khj000217@naver.com>
2022-10-09 17:58:17 +0900

    feat/#67/scss styled-components로 바꾸는중

--- frontend/src/Pages/BR_Pages/BR_ReviewReg.jsx
+++ frontend/src/Pages/BR_Pages/BR_ReviewReg.jsx
@@ -4,37 +4,76 @@ import './BR_ReviewReg.scss';
 import RegButton from '../../Components/Btn.jsx';
 import Input from '../../Components/Btn.jsx';
 
-// const Wrap = styled.div`
-//   margin: 12rem auto 0rem auto;
-//   width: 55rem;
-//   height: fit-content;
-//   background-color: rgb(218, 201, 194);
-//   padding: 3rem;
-// `;
+const Wrap = styled.div`
+  margin: 12rem auto 0rem auto;
+  width: 55rem;
+  height: fit-content;
+  background-color: rgb(218, 201, 194);
+  padding: 3rem;
+  box-sizing: content-box;
+`;
 
-// const Image = styled.div`
-//   width: 12rem;
-//   height: 15rem;
-//   background-color: #ffffff;
-//   display: flex;
-// `;
+const WrapContent = styled.div`
+  display: flex;
+`;
 
-// const OneLineReview = styled.div`
-//   width: 22rem;
-//   height: 5rem;
-//   background-color: white;
-//   margin-left: 30rem;
-//   border-radius: 0.2rem;
-// `;
+const BookImage = styled.div`
+  width: 12rem;
+  height: 15rem;
+  background-color: #ffffff;
+`;
 
+const WrapReview = styled.div`
+  margin-left: 3rem;
+`;
+const OneLineReview = styled.div`
+  width: 30rem;
+  height: 3rem;
+  background-color: white;
+  border-radius: 0.2rem;
+`;
+
+const WrapDetailReview = styled.div`
+  width: fit-content;
+  height: fit-content;
+  padding: 1rem;
+  background-color: rgb(186, 176, 176);
+  border-radius: 0.2rem;
+  margin-top: 2rem;
+`;
+
+const WrapStarScore = styled.div`
+  display: flex;
+`;
+
+const StarScore = styled.div`
+  margin-left: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const DetailReview = styled.div`
+  width: 30rem;
+  height: 5rem;
+  background-color: white;
+  border-radius: 0.2rem;
+  margin: auto;
+`;
 function BR_ReviewReg() {
   return (
     <div>
-      {/* <Wrap>
-        <Image></Image>
-        <OneLineReview></OneLineReview>
-      </Wrap> */}
-      <div className='wrap'>
+      <Wrap>
+        <WrapContent>
+          <BookImage></BookImage>
+          <WrapReview>
+            <OneLineReview></OneLineReview>
+            <WrapDetailReview>
+              <WrapStarScore></WrapStarScore>
+              <DetailReview></DetailReview>
+            </WrapDetailReview>
+          </WrapReview>
+        </WrapContent>
+      </Wrap>
+      {/* <div className='wrap'>
         <div className='wrap_content'>
           <div className='book_image'></div>
           <div className='wrap_review'>
@@ -56,13 +95,6 @@ function BR_ReviewReg() {
                 height={'5rem'}
                 placeholder={'한줄평'}
               />
-              {/* <RegButton
-                className='RegButton'
-                background={'rgb(179, 146, 131)'}
-                width={'5rem'}
-                height={'2rem'}
-                name={'등 록'}
-              /> */}
               <RegButton
                 background={'rgba(179, 146, 131)'}
                 width={'5rem'}
@@ -72,7 +104,7 @@ function BR_ReviewReg() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
