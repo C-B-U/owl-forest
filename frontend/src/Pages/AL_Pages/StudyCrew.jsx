cd0c7695b0b951df66c0b366f6e9127c05ccfcfd
suhyeon <suhyeon3484@naver.com>
2022-10-10 00:04:12 +0900

    feat: make studypage

--- frontend/src/Pages/AL_Pages/StudyCrew.jsx
+++ frontend/src/Pages/AL_Pages/StudyCrew.jsx
@@ -3,6 +3,8 @@ import styled, { ThemeProvider } from 'styled-components';
 import theme from '../../Components/Color';
 import { palette } from 'styled-tools';
 import Logo10 from '../../Image/Logo10.png';
+import Input from '../../Components/Input';
+// import Scroll from '../../Components/Scroll';
 
 const HeaderWrapper = styled.div`
   height: 4rem;
@@ -30,6 +32,45 @@ const Title = styled.h1`
   display: flex;
 `;
 
+const MainWrap = styled.div`
+  display: flex;
+  background-color: ${palette('PsCocoa', 0)};
+  background-size: cover;
+`;
+
+const ListWrap = styled.div`
+  display: inline-block;
+`;
+
+const OutLine = styled.div`
+  width: 90rem;
+  height: 45rem;
+  border-radius: 1rem;
+  margin-left: 4rem;
+  margin-right: 4rem;
+  background-color: ${palette('PsLightBrown')};
+  display: inline-block;
+`;
+
+const StudyIndex = styled.div`
+  width: 200rem;
+  display: flex;
+  margin: 1rem;
+  align-items: center;
+`;
+
+const StudyHeader = styled.header`
+  width: 10rem;
+  height: 4rem;
+  margin-top: 1rem;
+  padding-left: 4rem;
+  font-size: 20pt;
+  font-weight: bold;
+  align-items: center;
+  justify-content: left;
+  display: flex;
+`;
+
 function StudyCrew() {
   return (
     <div>
@@ -38,6 +79,44 @@ function StudyCrew() {
           <Logo />
           <Title>활동일지 서비스</Title>
         </HeaderWrapper>
+        <MainWrap>
+          <ListWrap>
+            <StudyHeader>스터디 수정</StudyHeader>
+            <OutLine>
+              {/* <Scroll> */}
+              <StudyIndex>
+                <StudyHeader>스터디 개요</StudyHeader>
+                <Input width={'40rem'} height={'10rem'} />
+              </StudyIndex>
+              <StudyIndex>
+                <StudyHeader>팀장</StudyHeader>
+                <Input width={'7rem'} height={'4rem'} />
+                <Input width={'30rem'} height={'4rem'} />
+              </StudyIndex>
+              <StudyIndex>
+                <StudyHeader>팀원</StudyHeader>
+                <Input width={'7rem'} height={'4rem'} />
+                <Input width={'30rem'} height={'4rem'} />
+              </StudyIndex>
+              <StudyIndex>
+                <StudyHeader></StudyHeader>
+                <Input width={'7rem'} height={'4rem'} />
+                <Input width={'30rem'} height={'4rem'} />
+              </StudyIndex>
+              <StudyIndex>
+                <StudyHeader></StudyHeader>
+                <Input width={'7rem'} height={'4rem'} />
+                <Input width={'30rem'} height={'4rem'} />
+              </StudyIndex>
+              <StudyIndex>
+                <StudyHeader></StudyHeader>
+                <Input width={'7rem'} height={'4rem'} />
+                <Input width={'30rem'} height={'4rem'} />
+              </StudyIndex>
+              {/* </Scroll> */}
+            </OutLine>
+          </ListWrap>
+        </MainWrap>
       </ThemeProvider>
     </div>
   );
