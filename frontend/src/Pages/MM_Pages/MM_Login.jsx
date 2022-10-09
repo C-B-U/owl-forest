b45cbb5b1ceb6438239014444619e87d5fc54bb6
nEunseo <nes020781@gmail.com>
2022-10-10 02:37:49 +0900

    feat/#59 레이아웃 컴포넌트 제작 끝

--- frontend/src/Pages/MM_Pages/MM_Login.jsx
+++ frontend/src/Pages/MM_Pages/MM_Login.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import imgfile from '../../Image/Loginlogo.png';
+
+//1rem = 대략 16px
+
+const Mainlogo = styled.img`
+  width: 10rem;
+  height: 10rem;
+  display: flex;
+  padding: 1.2rem;
+`;
+
+
+const LoginWrap = styled.div`
+  position : absolute;    
+  width :100%;
+  height: 100%;
+  align-items:center;
+  justify-content:center;
+  flex-direction: column;
+  display: flex;
+  text-align: center;
+  margin : 0 auto;    
+  background-color: rgba(248, 248, 248, 1);
+`;
+
+const InputWrap = styled.div`
+  margin-top: 0.7rem;
+  margin-bottom: 0.7rem;
+  display:flex;
+  flex-direction: column;
+`;
+
+const InputButton = styled.input`
+  width: 18rem;
+  height: 2.8rem;
+  background-color: rgba(247, 232, 211, 1);
+  border-radius: 5rem;
+  padding: 0rem 1.5rem 0rem 1.5rem;
+  font-family: 'Noto Sans KR', sans-serif;
+  input:focus {
+    outline: none;
+  }
+  border: none;
+  line-height: 1rem;
+  margin-bottom: 1rem;
+`;
+
+
+const LogBtnWrap = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1.3rem;
+  white-space: pre;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
+
+
+const LoginButton = styled.button`
+  width: 15rem;
+  height: 2.8rem;
+  background-color: rgba(183, 227, 156, 1);
+  border-radius: 3rem;
+  padding: 0rem 1.5rem 0rem 0.5rem;
+  font-family: 'Noto Sans KR', sans-serif;
+  border: none;
+  font-size: 12pt;
+`;
+
+
+const LinkWrap = styled.div`
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  font-family: 'Noto Sans KR', sans-serif;
+  
+`;
+
 
 function MM_Login() {
+  //선언
   return (
-    <div>MM_Login</div>
-  )
-}
+    <LoginWrap>
+      
+      <Mainlogo src={imgfile}/>
 
-export default MM_Login
+      <InputWrap>
+        <InputButton placeholder='ID'/>
+        <InputButton type="password" placeholder='Password' />
+      </InputWrap>
+
+      <LogBtnWrap>
+        <LoginButton>로그인</LoginButton>
+      </LogBtnWrap>
+
+      <LinkWrap>
+        아직 계정이 없으신가요?&nbsp;&nbsp;
+        <Link to='/Reg' style={{ color: 'inherit', textDecoration: 'inherit' }}>
+          회원가입
+        </Link>
+      </LinkWrap>
+
+      <LinkWrap>
+        
+       <Link
+          to='/FindId'
+          style={{ color: 'inherit', textDecoration: 'inherit' }}
+        >아이디 찾기&emsp;&emsp;</Link>
+      
+        
+        <Link
+          to='/FindPw'
+          style={{ color: 'inherit', textDecoration: 'inherit' }}
+        >비밀번호 찾기</Link>
+       </LinkWrap>
+      
+    </LoginWrap>
+  );
+}
+export default MM_Login;
