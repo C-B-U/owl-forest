b247696cd843ad924cbab65a0db27f0ca6952814
김혜진 <khj000217@naver.com>
2022-10-09 23:11:34 +0900

    feat/#74/색칠놀이

--- frontend/src/Pages/BR_Pages/BR_ReviewReg.jsx
+++ frontend/src/Pages/BR_Pages/BR_ReviewReg.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import { palette } from 'styled-tools';
+import theme from '../../Components/Color';
+import Header from '../../Components/ActivityLog/Header';
 import RegButton from '../../Components/Btn.jsx';
 import Input from '../../Components/Input.jsx';
 
+const MainWrap = styled.div`
+  position: absolute;
+  background-color: ${palette('PsCocoa', 0)};
+  width: 100%;
+  height: calc(100% - 4rem);
+`;
+
 const Wrap = styled.div`
-  margin: 12rem auto 0rem auto;
+  margin: 8rem auto 0rem auto;
   width: fit-content;
   height: fit-content;
-  background-color: rgb(218, 201, 194);
+  background-color: ${palette('PsCocoa', 1)};
   padding: 3rem;
   box-sizing: content-box;
 `;
 
+const Title = styled.h1`
+  color: ${palette('PsYellow')};
+`;
+
 const WrapContent = styled.div`
   display: flex;
+
   /* margin: auto 0; */
 `;
 
@@ -37,7 +52,8 @@ const WrapDetailReview = styled.div`
   width: fit-content;
   height: 13.3rem;
   padding: 1rem;
-  background-color: rgb(186, 176, 176);
+  background-color: ${palette('PsLightBrown', 0)};
+  /* background-color: white; */
   border-radius: 0.2rem;
   margin-top: 1rem;
   /* float: right; */
@@ -58,38 +74,54 @@ const DetailReview = styled.div`
 
 const WrapRegButton = styled.div`
   float: right;
+  /* margin-right: auto; */
 `;
 
 function BR_ReviewReg() {
   return (
     <div>
-      <Wrap>
-        <WrapContent>
-          <BookImage />
-          <WrapReview>
-            <OneLineReview>
-              <Input width={'40rem'} height={'2.3rem'} placeholder='한줄평' />
-            </OneLineReview>
-            <WrapDetailReview>
-              <WrapStarScore>
-                <StarScore>난이도</StarScore>
-                <StarScore>평점</StarScore>
-              </WrapStarScore>
-              <DetailReview>
-                <Input width={'38rem'} height={'7rem'} placeholder='상세평' />
-              </DetailReview>
-              <WrapRegButton>
-                <RegButton
-                  background={'rgba(179, 146, 131)'}
-                  width={'5rem'}
-                  height={'2rem'}
-                  name='등 록'
-                />
-              </WrapRegButton>
-            </WrapDetailReview>
-          </WrapReview>
-        </WrapContent>
-      </Wrap>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <MainWrap>
+          <Wrap>
+            <Title>북리뷰 등록</Title>
+            <WrapContent>
+              <BookImage />
+              <WrapReview>
+                <OneLineReview>
+                  <Input
+                    width={'40rem'}
+                    height={'2.3rem'}
+                    placeholder='한줄평'
+                  />
+                </OneLineReview>
+                <WrapDetailReview>
+                  <WrapStarScore>
+                    <StarScore>난이도</StarScore>
+                    <StarScore>평점</StarScore>
+                  </WrapStarScore>
+                  <DetailReview>
+                    <Input
+                      width={'38rem'}
+                      height={'7rem'}
+                      placeholder='상세평'
+                    />
+                  </DetailReview>
+                  <WrapRegButton>
+                    <RegButton
+                      color={palette('PsYellow')}
+                      background={palette('PsBtn')}
+                      width={'5.5rem'}
+                      height={'2.5rem'}
+                      name='등 록'
+                    />
+                  </WrapRegButton>
+                </WrapDetailReview>
+              </WrapReview>
+            </WrapContent>
+          </Wrap>
+        </MainWrap>
+      </ThemeProvider>
     </div>
   );
 }
