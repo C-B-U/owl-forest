3ce6d23595645f4ec0440391d88557b9977f4e63
nEunseo <nes020781@gmail.com>
2022-10-10 23:02:54 +0900

    fix/#59 배경색 styled-tool로 받아오게 수정, 소셜로그인 코멘트 삽입

--- frontend/src/Pages/MM_Pages/MM_Login.jsx
+++ frontend/src/Pages/MM_Pages/MM_Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import { palette } from 'styled-tools'
 import { Link } from 'react-router-dom';
 import imgfile from '../../Image/Loginlogo.png';
 import githubbtn from '../../Image/githubbtn.png'
 import googlebtn from '../../Image/googlebtn.png'
 import kakaobtn from '../../Image/kakaobtn.png'
 import naverbtn from '../../Image/naverbtn.png'
+import theme from '../../Components/Color';
 
 //1rem = 대략 16px
 
@@ -27,24 +29,23 @@ const LoginWrap = styled.div`
   display: flex;
   text-align: center;
   margin : 0 auto;    
-  background-color: rgba(248, 248, 248, 1);
+  background-color: ${palette('PsWhite')};
 `;
 
 const InputWrap = styled.div`
-  margin-top: 0.7rem;
-  margin-bottom: 0.7rem;
-  display:flex;
+  margin: 0.7rem;
+  display: flex;
   flex-direction: column;
 `;
 
 const InputButton = styled.input`
   width: 18rem;
   height: 2.8rem;
-  background-color: rgba(247, 232, 211, 1);
+  background-color: ${palette('PsLightBrown')};
   border-radius: 5rem;
-  padding: 0rem 1.5rem 0rem 1.5rem;
+  padding: 0rem 1.5rem;
   font-family: 'Noto Sans KR', sans-serif;
-  input:focus {
+  &:focus {
     outline: none;
   }
   border: none;
@@ -64,7 +65,7 @@ const LogBtnWrap = styled.div`
 const LoginButton = styled.button`
   width: 15rem;
   height: 2.8rem;
-  background-color: rgba(183, 227, 156, 1);
+  background-color: ${palette('PsGreen')};
   border-radius: 3rem;
   padding: 0rem 1.5rem 0rem 0.5rem;
   font-family: 'Noto Sans KR', sans-serif;
@@ -83,7 +84,7 @@ const LinkWrap = styled.div`
 
 const Line = styled.div`
   margin-top: 1rem; //위의 아이디/비번 찾기와의 간격 띄우기 위함
-  
+  margin-bottom: 0.5rem;
 `;
 
 
@@ -103,52 +104,62 @@ const SocialImg = styled.img`
 function MM_Login() {
   //선언
   return (
-    <LoginWrap>
-      
-      <Mainlogo src={imgfile}/>
+    <div>
+      <ThemeProvider theme={theme}>
+        <LoginWrap>
+          
+          <Mainlogo src={imgfile}/>
 
-      <InputWrap>
-        <InputButton placeholder='ID'/>
-        <InputButton type="password" placeholder='Password' />
-      </InputWrap>
+          <InputWrap>
+            <InputButton placeholder='ID'/>
+            <InputButton type="password" placeholder='Password' />
+          </InputWrap>
 
-      <LogBtnWrap>
-        <LoginButton>로그인</LoginButton>
-      </LogBtnWrap>
+          <LogBtnWrap>
+            <LoginButton>로그인</LoginButton>
+          </LogBtnWrap>
 
-      <LinkWrap>
-        아직 계정이 없으신가요?&nbsp;&nbsp;
-        <Link to='/Reg' style={{ color: 'inherit', textDecoration: 'inherit' }}>
-          회원가입
-        </Link>
-      </LinkWrap>
+          <LinkWrap>
+            아직 계정이 없으신가요?&nbsp;&nbsp;
+            <Link to='/Reg' style={{ color: 'inherit', textDecoration: 'inherit' }}>
+              회원가입
+            </Link>
+          </LinkWrap>
 
-      <LinkWrap>
-        
-       <Link
-          to='/FindId'
-          style={{ color: 'inherit', textDecoration: 'inherit' }}
-        >아이디 찾기&emsp;&emsp;</Link>
-      
-        
-        <Link
-          to='/FindPw'
-          style={{ color: 'inherit', textDecoration: 'inherit' }}
-        >비밀번호 찾기</Link>
-       </LinkWrap>
+          <LinkWrap>
+            
+          <Link
+              to='/FindId'
+              style={{ color: 'inherit', textDecoration: 'inherit' }}
+            >아이디 찾기&emsp;&emsp;</Link>
+          
+            
+            <Link
+              to='/FindPw'
+              style={{ color: 'inherit', textDecoration: 'inherit' }}
+            >비밀번호 찾기</Link>
+          </LinkWrap>
 
-       <Line>
-        <hr style={{width: '25rem', }}></hr>
-       </Line>
-    <SocialWrap>
-      <SocialImg style={{margin: '1rem'}} src={googlebtn}/>
-      <SocialImg style={{margin: '1rem'}} src={githubbtn}/>
-      <SocialImg style={{margin: '1rem'}} src={kakaobtn}/>
-      <SocialImg style={{margin: '1rem'}} src={naverbtn}/>
-    </SocialWrap>
+          <Line>
+            <hr style={{width: '25rem', }}></hr>
+          </Line>
 
+          <LinkWrap>
+            소셜 계정으로 간단하게 로그인하세요!
+          </LinkWrap>
 
-    </LoginWrap>
+        <SocialWrap>
+          <SocialImg style={{margin: '1rem'}} src={googlebtn}/>
+          <SocialImg style={{margin: '1rem'}} src={githubbtn}/>
+          <SocialImg style={{margin: '1rem'}} src={kakaobtn}/>
+          <SocialImg style={{margin: '1rem'}} src={naverbtn}/>
+        </SocialWrap>
+
+        </LoginWrap>
+
+    </ThemeProvider>
+    </div>
+    
   );
 }
 export default MM_Login;
