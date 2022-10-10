62ed3e9936a58834b7aed0321f9ef56b845023f2
suhyeon <suhyeon3484@naver.com>
2022-10-11 00:21:28 +0900

    feat: plusbutton

--- frontend/src/Pages/AL_Pages/StudyCrew.jsx
+++ frontend/src/Pages/AL_Pages/StudyCrew.jsx
@@ -5,6 +5,7 @@ import { palette } from 'styled-tools';
 import Logo10 from '../../Image/Logo10.png';
 import Input from '../../Components/Input';
 import Btn from '../../Components/Btn';
+import Plus from '../../Image/PlusButton.png';
 // import Scroll from '../../Components/Scroll';
 
 const HeaderWrapper = styled.div`
@@ -48,25 +49,36 @@ const StudyWrap = styled.div`
 `;
 
 const OutLine = styled.div`
-  width: 60rem;
-  height: 48rem;
+  width: 100rem;
+  height: 40rem;
   border-radius: 1rem;
   margin: auto;
+  padding: 4rem;
   background-color: ${palette('PsLightBrown')};
   display: inline-block;
   text-align: center;
 `;
 
+// const Study = styled.div`
+//   display: flex;
+// `;
+
+const Member = styled.div`
+  margin: 0.2rem;
+`;
+
 const StudyIndex = styled.div`
   display: flex;
   margin: 1rem;
+  margin-right: 10rem;
   align-items: center;
+  justify-content: center;
 `;
 
 const StudyHeader = styled.header`
   width: 60rem;
   height: 4rem;
-  margin-top: 1rem;
+  margin-top: 4rem;
   margin-left: 1rem;
   font-size: 20pt;
   font-weight: bold;
@@ -87,6 +99,18 @@ const Studylist = styled.header`
   display: flex;
 `;
 
+const PlusBtn = styled.button`
+  background-image: url(${Plus});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 2rem;
+  background-color: ${palette('PsLightBrown')};
+  border: 0em;
+  width: 3rem;
+  height: 3rem;
+  margin-left: 0.5rem;
+`;
+
 function StudyCrew() {
   return (
     <div>
@@ -99,37 +123,41 @@ function StudyCrew() {
           <StudyWrap>
             <StudyHeader>스터디 수정</StudyHeader>
             <OutLine>
-              {/* <Scroll> */}
+              {/* <Study> */}
               <StudyIndex>
                 <Studylist>스터디 개요</Studylist>
-                <Input width={'40rem'} height={'10rem'} />
+                <Input width={'40.5rem'} height={'8rem'} />
               </StudyIndex>
-              <StudyIndex>
-                <Studylist>팀장</Studylist>
-                <Input width={'7rem'} height={'3rem'} />
-                <Input width={'30rem'} height={'3rem'} />
-              </StudyIndex>
-              <StudyIndex>
-                <Studylist>팀원</Studylist>
-                <Input width={'7rem'} height={'3rem'} />
-                <Input width={'30rem'} height={'3rem'} />
-              </StudyIndex>
-              <StudyIndex>
-                <Studylist></Studylist>
-                <Input width={'7rem'} height={'3rem'} />
-                <Input width={'30rem'} height={'3rem'} />
-              </StudyIndex>
-              <StudyIndex>
-                <Studylist></Studylist>
-                <Input width={'7rem'} height={'3rem'} />
-                <Input width={'30rem'} height={'3rem'} />
-              </StudyIndex>
-              <StudyIndex>
-                <Studylist></Studylist>
-                <Input width={'7rem'} height={'3rem'} />
-                <Input width={'30rem'} height={'3rem'} />
-              </StudyIndex>
-              {/* </Scroll> */}
+              <Member>
+                <StudyIndex>
+                  <Studylist>팀장</Studylist>
+                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
+                  <Input width={'30rem'} height={'3rem'} />
+                </StudyIndex>
+                <StudyIndex>
+                  <Studylist>팀원</Studylist>
+                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
+                  <Input width={'30rem'} height={'3rem'} />
+                  {/* <PlusBtn></PlusBtn> */}
+                </StudyIndex>
+                <StudyIndex>
+                  <Studylist></Studylist>
+                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
+                  <Input width={'30rem'} height={'3rem'} />
+                </StudyIndex>
+                <StudyIndex>
+                  <Studylist></Studylist>
+                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
+                  <Input width={'30rem'} height={'3rem'} />
+                </StudyIndex>
+                <StudyIndex>
+                  <Studylist></Studylist>
+                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
+                  <Input width={'30rem'} height={'3rem'} />
+                </StudyIndex>
+              </Member>
+              {/* </Study> */}
+              <PlusBtn></PlusBtn>
               <Btn
                 background={palette('PsBtn')}
                 color={palette('PsYellow')}
