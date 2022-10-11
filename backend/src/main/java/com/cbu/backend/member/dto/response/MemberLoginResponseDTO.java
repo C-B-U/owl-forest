a930ac2edf94ad51db1133d22d40e3df7d3ef88c
rt3310 <btac3310@gmail.com>
2022-10-11 12:52:39 +0900

    feat: #62 Member Login Request, Response dto 구현

---
+++ backend/src/main/java/com/cbu/backend/member/dto/response/MemberLoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.cbu.backend.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberLoginResponseDTO {
+    private String token;
+}
