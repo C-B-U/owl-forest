b2cd9073dbc653fb055050ab8127a3c4e4923133
김연진 <love_homebody@naver.com>
2022-10-11 20:41:55 +0900

    feat: add studyActivityLog entity

--- backend/src/main/java/com/cbu/backend/studyactivitylog/entity/StudyActivityLog.java
+++ backend/src/main/java/com/cbu/backend/studyactivitylog/entity/StudyActivityLog.java
@@ -1,4 +1,36 @@
 package com.cbu.backend.studyactivitylog.entity;
 
+import com.cbu.backend.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Table(name = "studyActivityLog")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyActivityLog {
+    @Id
+    @GeneratedValue
+    private Long Id;
+
+    private Integer count;
+    
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime startTime;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime endTime;
+
+    private String place;
+
+    @OneToMany(mappedBy = "member")
+    private List<Member> teamMembers = new ArrayList<>();
+
+    private String activityDetail;
 }
