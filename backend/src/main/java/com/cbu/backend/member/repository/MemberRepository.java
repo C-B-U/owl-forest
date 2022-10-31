dc04082e5621720acfb487d32ed7871d78d57295
yeonjy <yeonjypaper@gmail.com>
2022-11-01 00:33:46 +0900

    feat: repository 구현

--- backend/src/main/java/com/cbu/backend/member/repository/MemberRepository.java
+++ backend/src/main/java/com/cbu/backend/member/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import com.cbu.backend.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByAccountId(String accountId);
 }
