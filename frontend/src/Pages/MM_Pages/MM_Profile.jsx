00c1a54514d767557e9b9f834fa64b3f5c88d4b2
minseok <skyfox001015@naver.com>
2022-10-31 17:44:30 +0900

    feat:#112:왼쪽 오른쪽 레이아웃 나눔

--- frontend/src/Pages/MM_Pages/MM_Profile.jsx
+++ frontend/src/Pages/MM_Pages/MM_Profile.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { palette } from 'styled-tools';
+import theme from '../../Components/Color';
+import { InputStyle } from '../../Components/Input'
+import Btn from '../../Components/Btn'
+import imgfile from '../../Image/Logo10.png';
+
+const Profileimg = styled.img`
+  width:5rem;
+  height:5rem;
+  border-radius :100%;
+`;
+const All = styled.div`
+  display:flex;
+  justify-content: space-around;
+`
+const Left = styled.div`
+  display:flex;
+`
+const Right = styled.div`
+  display:flex;
+`
+
 
 function MM_Profile() {
   return (
-    <div>MM_Profile</div>
+    <div>
+      <All>
+        <Left>
+          <Profileimg src={imgfile}></Profileimg>
+        </Left>
+        <Right>
+          <Profileimg src={imgfile}></Profileimg>
+        </Right>
+      </All>
+    </div>
+
   )
 }
 
