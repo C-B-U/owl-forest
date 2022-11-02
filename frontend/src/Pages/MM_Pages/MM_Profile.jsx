54cb4d449e2e6fe64dd93796bdaa0883ec019572
minseok <skyfox001015@naver.com>
2022-11-02 16:01:08 +0900

    feat:#112:왼쪽 오른쪽 비율 1:5로 맞추로 왼쪽은 fixed시킴

--- frontend/src/Pages/MM_Pages/MM_Profile.jsx
+++ frontend/src/Pages/MM_Pages/MM_Profile.jsx
@@ -5,46 +5,45 @@ import theme from '../../Components/Color';
 import { InputStyle } from '../../Components/Input'
 import imgfile from '../../Image/Logo10.png';
 
+
+const Wrap = styled.div`
+  position:relative;
+  display:flex;
+`
+const LeftFixedWrap = styled.div`
+  height:10rem;
+  width:10rem;
+  background-color:blue;
+  position:fixed;
+`
+const LeftWrap = styled.div`
+  height:50rem;
+  width:10rem;
+  flex-grow:1;
+  border: 1px solid gray;
+  margin-right:1rem;
+`
+
+const RightWrap = styled.div`
+  border: 1px solid gray;
+  flex-grow:5;
+`
 const Profileimg = styled.img`
   width:5rem;
   height:5rem;
   border-radius :100%;
 `;
-const Wrap = styled.div`
-  display:flex;
-  flex-wrap:nowrap;
-`
-const Left = styled.div`
-  flex-grow:1;
-`
-
-const Right = styled.div`
-  flex-grow:3;
-`
-const Square = styled.div`
-  width : 10rem;
-  height : 30rem;
-  background-color :tomato;
-  position:fixed;
-`
-const Square2 = styled.div`
-  width : 10rem;
-  height : 30rem;
-  background-color :blue;
-`
 
 function MM_Profile() {
   return (
     <div>
       <Wrap>
-        <Left>
-          <Profileimg src={imgfile}></Profileimg>
-          <Square />
-          <Square2 />
-        </Left>
-        <Right>
-          <Profileimg src={imgfile}></Profileimg>
-        </Right>
+        <LeftWrap>
+          <LeftFixedWrap>
+          </LeftFixedWrap>
+        </LeftWrap>
+        <RightWrap>
+        </RightWrap>
       </Wrap>
     </div>
 
