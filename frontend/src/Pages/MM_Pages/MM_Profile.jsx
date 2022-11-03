085032ffe361d8326f0645aa029fa7d9896ac2ba
minseok <skyfox001015@naver.com>
2022-11-03 18:22:07 +0900

    feat:#112:TechBox추가 위치 조정 완료

--- frontend/src/Pages/MM_Pages/MM_Profile.jsx
+++ frontend/src/Pages/MM_Pages/MM_Profile.jsx
@@ -44,12 +44,38 @@ const Profileimg = styled.img`    //자신의 프로필 이미지
 const Intro = styled.div`   //introduction 줄여서 intro ->한 줄 소개하는 영역
   width:18rem;
   height:5rem;
-  background-color:${palette('PsBtn')};
+  background-color:${palette('PsCocoa')};
+  border-radius:1rem;
   margin: auto;       //블록요소인 div가운데 정렬
   font-family: 'Noto Sans KR', sans-serif;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  display: flex;                  //
+  align-items: center;            //요소 내부 세로 정렬
+  justify-content: center;        //
+  @media (max-width:60rem){   //화면 바뀌면 사이즈 키움
+      width:20rem;
+  }
+`
+const TechBox = styled.div`     //Tech Stack을 나타내는 박스
+  width:14rem;
+  height:18rem;
+  background-color:${palette('PsLightBrown')};
+  margin: auto;       //블록요소인 div가운데 정렬
+  margin-top : 3rem;
+  border-radius:2rem;
+  display:flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+
+  @media (max-width:60rem){   //화면 바뀌면 사이즈 키움
+      width:20rem;
+  }
+`
+const Tech = styled.div`
+  height : 1.5rem;
+  background-color:white;
+  margin : 0.7rem 0.5rem;
+  padding : 0.3rem 0.5rem;
+  border-radius : 1rem;
 `
 
 function MM_Profile() {
@@ -60,6 +86,12 @@ function MM_Profile() {
           <LeftWrap>
             <Profileimg src={imgfile} />
             <Intro>짧은 자기소개를 해주세요.</Intro>
+            <TechBox>
+              <Tech>Java</Tech>
+              <Tech>Python</Tech>
+              <Tech>C++</Tech>
+              <Tech>React</Tech>
+            </TechBox>
           </LeftWrap>
           <RightWrap>
           </RightWrap>
