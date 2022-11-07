3bf878bfb1e3bd50e26b4be97059d60995e26c26
seongeunbong <bongfam2@naver.com>
2022-11-07 12:21:25 +0900

    feat/#114 MemList List 구현 및 Profile 스크롤 고정

--- frontend/src/Pages/MM_Pages/MM_MemList.jsx
+++ frontend/src/Pages/MM_Pages/MM_MemList.jsx
@@ -16,17 +16,20 @@ const MainWrap = styled.div`
 //리스트에서 사용
 const ListWrap = styled.div`
   padding-left: auto;
-  display: flex;
+  // display: flex;
 `;
 
-const Rightlineup = styled.div`
-  float: right;
-  padding-left: 60rem;
+const LineUp = styled.div`
+  flexwrap: wrap;
+  position: sticky;
+  margin-left: auto;
 `;
 
 //마이페이지, 검색의 가로 정렬을 위해 사용
 const SearchWrap = styled.div`
-  // display: flex;
+  display: flex;
+  // margin-left: auto;
+  // padding-right: 1rem;
 `;
 
 const Test = styled.div``;
@@ -56,6 +59,7 @@ const InfoButton = styled.button`
   margin-top: 2rem;
   margin-left: 1.5rem;
   font-weight: bolder;
+  margin-right: 48.5rem;
 `;
 
 const SearchBox = styled.div`
@@ -64,27 +68,93 @@ const SearchBox = styled.div`
   background-color: ${palette('PsGreen')};
   border-radius: 3rem;
   font-family: 'Noto Sans KR', sans-serif;
-  border: none;
+  border: 0.5rem;
   font-size: 12pt;
   margin-top: 2rem;
-  margin-left: 1.5rem;
   font-weight: bolder;
+  margin-left: auto;
+  margin-right: 1rem;
 `;
-
+const MemList = styled.div`
+  width: 75rem;
+  height: 1rem;
+  border-radius: 3rem;
+  margin-top: 0.8rem;
+  padding: 1.5rem;
+  display: flex;
+  background-color: ${palette('PsLightBrown')};
+  margin-left: 1.5rem;
+`;
+const MemListInput = styled.div`
+  font-size: 10pt;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+`;
+const Wrap = styled.div`
+  position: fixed;
+`;
 function MM_MemList() {
   return (
     <div>
       <ThemeProvider theme={theme}>
         <MainWrap>
-          <MLProfile />
+          <Wrap>
+            <MLProfile />
+          </Wrap>
 
-          <SearchWrap>
-            <InfoButton>마이 페이지</InfoButton>
-
-            <Rightlineup>
+          <LineUp>
+            <SearchWrap>
+              <InfoButton>마이페이지</InfoButton>
               <SearchBox></SearchBox>
-            </Rightlineup>
-          </SearchWrap>
+            </SearchWrap>
+
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+            <MemList>
+              <MemListInput>안녕하세요</MemListInput>
+            </MemList>
+          </LineUp>
         </MainWrap>
       </ThemeProvider>
     </div>
