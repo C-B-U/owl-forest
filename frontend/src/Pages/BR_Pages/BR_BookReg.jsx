d4b9e1d419ffbab703443c3662c670e09fb6268f
yeahzxnn <yejin0621@tukorea.ac.kr>
2022-11-14 20:35:58 +0900

    feat:#115:디자인 틀 잡아두기
    이제 상세내용 작업해야합니다!

--- frontend/src/Pages/BR_Pages/BR_BookReg.jsx
+++ frontend/src/Pages/BR_Pages/BR_BookReg.jsx
@@ -2,145 +2,140 @@ import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { palette } from 'styled-tools';
 import theme from '../../Components/Color';
+import Header from '../../Components/ActivityLog/Header';
 import SearchBar from '../../Components/Input';
 import SearchIcon from '../../Image/SearchIcon.png';
 import SearchButton from '../../Components/Btn';
+import Input from '../../Components/Input.jsx';
 
 const Wrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: ${palette('PsCocoa')};
+  margin: 8rem auto 0rem auto;
+  // width: fit-content;
+  // height: fit-content;
+  background-color: #DAC9C2;
+  padding: 3rem;
+  box-sizing: content-box;
 `;
 
-const WrapPopupBackground = styled.div`
-  position: absolute;
-  width: 100vw;
-  height: 100vh;
-  /* background-color: rgba(0, 0, 0, 0.5); */
-  background-color: rgba(255, 255, 255, 0.2);
+const WrapContent = styled.div`
+  display: flex;
+
+  /* margin: auto 0; */
 `;
+
+const BookImage = styled.div`
+  width: 15rem;
+  height: 19rem;
+  background-color: #ffffff;
+
+`;
+
+const Title = styled.h1`
+  color: ${palette('Black')};
+  text-align: left;
+
+`;
+
+const CalendarImage = styled.div`
+    width : 15rem;
+    height: 19rem;
+    background-color: #aaaa;
+
+`;
+
+
+// const Title = styled.h1`
+//   color: ${palette('PsYellow')};
+// `;
+
+// const WrapPopupBackground = styled.div`
+//   position: absolute;
+//   width: 100vw;
+//   height: 100vh;
+//   /* background-color: rgba(0, 0, 0, 0.5); */
+//   background-color: rgba(255, 255, 255, 0.2);
+// `;
 
 // Wrap~~ --> ~~~Wrap으로 이름 바꾸기
 
 // x 버튼 구현
 // input 안으로 돋보기 버튼 넣기
 
-const WrapPopup = styled.div`
-  position: absolute;
-  width: 60rem;
-  height: 35rem;
-  background-color: ${palette('PsCocoa', 1)};
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius: 1rem;
-`;
+// const RegisterWrap
 
-const ISBNSearch = styled.div`
-  font-size: 3rem;
-  margin-top: 4rem;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-`;
+// const WrapPopup = styled.div`
+//   position: absolute;
+//   width: 60rem;
+//   height: 35rem;
+//   background-color: ${palette('PsCocoa', 1)};
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   border-radius: 1rem;
+// `;
 
-const WrapSearchbar = styled.div`
-  /* border: 1px solid black; */
-  margin-top: 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+// const Title = styled.div`
+//   width: 15rem;
+//   white-space: normal;
+//   text-align: center;
+//   /* border: 1px solid green; */
+//   /* word-break: break-all; */
+// `;
 
-const SearchForm = styled.form`
-  background-image: url(${SearchIcon});
-  background-position: right;
-  width: 40rem;
-  height: 3.2rem;
-  background-size: 3rem;
-  background-repeat: no-repeat;
-`;
+// const Writer = styled.div`
+//   width: 12rem;
+//   white-space: normal;
+//   /* word-break: break-all; */
+//   /* border: 1px solid green; */
+//   margin-left: 1rem;
+//   text-align: center;
+// `;
 
-const WrapSearchIcon = styled.div`
-  height: 3rem;
-  width: 3rem;
-  background-size: 3rem;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-image: url(${SearchIcon});
-`;
-
-const WrapList = styled.div`
-  /* border: 1px solid black; */
-  height: fit-content;
-  width: fit-content;
-  margin: 2rem auto;
-`;
-
-const List = styled.div`
-  width: 40rem;
-  height: fit-content;
-  background-color: white;
-  margin-bottom: 1rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  box-sizing: content-box;
-  padding: 0.7rem;
-  justify-items: center;
-  border-radius: 0.2rem;
-`;
-
-const Title = styled.div`
-  width: 15rem;
-  white-space: normal;
-  text-align: center;
-  /* border: 1px solid green; */
-  /* word-break: break-all; */
-`;
-const Writer = styled.div`
-  width: 12rem;
-  white-space: normal;
-  /* word-break: break-all; */
-  /* border: 1px solid green; */
-  margin-left: 1rem;
-  text-align: center;
-`;
-const Publisher = styled.div`
-  width: 10rem;
-  white-space: normal;
-  word-break: break-all;
-  /* border: 1px solid green; */
-  margin-left: 1rem;
-  text-align: center;
-`;
-const ReleaseDate = styled.div`
-  width: 8rem;
-  white-space: normal;
-  word-break: break-all;
-  /* border: 1px solid green; */
-  margin-left: 1rem;
-  text-align: center;
-`;
+// const Publisher = styled.div`
+//   width: 10rem;
+//   white-space: normal;
+//   word-break: break-all;
+//   /* border: 1px solid green; */
+//   margin-left: 1rem;
+//   text-align: center;
+// `;
+// const ReleaseDate = styled.div`
+//   width: 8rem;
+//   white-space: normal;
+//   word-break: break-all;
+//   /* border: 1px solid green; */
+//   margin-left: 1rem;
+//   text-align: center;
+// `;
 
 function BR_BookReg() {
   return (
+    <div>
     <ThemeProvider theme={theme}>
-      <WrapPopupBackground>
+      <Header />
+      <Wrap>
+        <Title>도서등록</Title>
+        <CalendarImage></CalendarImage>
+        <WrapContent>
+          <BookImage />
+
+        </WrapContent>
+      </Wrap>
+      
+      {/* <WrapPopupBackground>
         <WrapPopup>
           <ISBNSearch>ISBN 검색</ISBNSearch>
           <WrapSearchbar>
             {/* <SearchForm></SearchForm> */}
-            <SearchBar
+            {/* <SearchBar
               width={'40rem'}
               height={'3.2rem'}
               placeholder='ISBN을 적어주세요.'
             ></SearchBar>
             <WrapSearchIcon />
-          </WrapSearchbar>
+          // </WrapSearchbar> */}
 
-          <WrapList>
+          {/* <WrapList>
             <List>
               <Title>프로그래밍 대회에서 배우는 알고리즘 문제 해결 전략1</Title>
               <Writer>천인국, 공용해, 하상호</Writer>
@@ -154,12 +149,15 @@ function BR_BookReg() {
               <Publisher>한빛아카데미</Publisher>
               <ReleaseDate>2022.02.22</ReleaseDate>
             </List>
+
           </WrapList>
         </WrapPopup>
-      </WrapPopupBackground>
+      </WrapPopupBackground> */}
 
       <Wrap></Wrap>
     </ThemeProvider>
+  </div>
+    
   );
 }
 
