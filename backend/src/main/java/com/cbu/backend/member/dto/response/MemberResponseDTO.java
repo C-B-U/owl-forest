a18247c4d010bb60a23c64ee8d6c3f67849d78fb
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-14 22:32:01 +0900

    feat: MemberResponse JsonView 적용

--- backend/src/main/java/com/cbu/backend/member/dto/response/MemberResponseDTO.java
+++ backend/src/main/java/com/cbu/backend/member/dto/response/MemberResponseDTO.java
@@ -1,13 +1,42 @@
 package com.cbu.backend.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class MemberResponseDTO {
+    @JsonView(MemberView.Normal.class)
     private Long id;
-    // TODO Member 구현 이후 추가
+    @JsonView(MemberView.Normal.class)
+    private String name;
+    @JsonView(MemberView.AdminExtra.class)
+    private String accountId;
+    @JsonView(MemberView.NormalExtra.class)
+    private String email;
+    @JsonView(MemberView.Normal.class)
+    private Integer grade;
+    @JsonView(MemberView.NormalExtra.class)
+    private String major;
+    @JsonView(MemberView.AdminExtra.class)
+    private Integer studentId;
+    @JsonView(MemberView.Normal.class)
+    private Integer generation;
+    @JsonView(MemberView.NormalExtra.class)
+    private String blogUrl;
+    @JsonView(MemberView.NormalExtra.class)
+    private String githubId;
+    @JsonView(MemberView.Normal.class)
+    private String profileUrl;
+    @JsonView(MemberView.AdminExtra.class)
+    private String phoneNumber;
+    @JsonView(MemberView.NormalExtra.class)
+    private String introduction;
+    @JsonView(MemberView.Normal.class)
+    private List<String> interestTags;
 }
--- backend/src/main/java/com/cbu/backend/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/cbu/backend/member/mapper/MemberMapper.java
@@ -2,13 +2,37 @@ package com.cbu.backend.member.mapper;
 
 import com.cbu.backend.global.Mapper;
 import com.cbu.backend.member.dto.response.MemberResponseDTO;
+import com.cbu.backend.member.dto.response.MemberView;
 import com.cbu.backend.member.entity.Member;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 
 @Mapper
+@RequiredArgsConstructor
 public class MemberMapper {
+    private final ObjectMapper objectMapper;
+
+    public String toJsonString(Member member, Class<MemberView> memberViewClass) throws JsonProcessingException {
+        return objectMapper.writerWithView(memberViewClass).writeValueAsString(member);
+    }
+
     public MemberResponseDTO toDto(Member member) {
         return MemberResponseDTO.builder()
                 .id(member.getId())
+                .name(member.getName())
+                .accountId(member.getEmail())
+                .email(member.getEmail())
+                .grade(member.getGrade())
+                .major(member.getMajor())
+                .studentId(member.getStudentId())
+                .generation(member.getGeneration())
+                .blogUrl(member.getBlogUrl())
+                .githubId(member.getGithubId())
+                .profileUrl(member.getProfileUrl())
+                .phoneNumber(member.getPhoneNumber())
+                .introduction(member.getIntroduction())
                 .build();
+
     }
 }
