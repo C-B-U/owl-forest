657530f86425b974afd0956a7b5f5ccadd0f05b0
김연진 <love_homebody@naver.com>
2022-11-15 22:21:49 +0900

    feat: delete controller 구현 임시저장

--- backend/src/main/java/com/cbu/backend/studyactivitylog/controller/StudyActivityLogController.java
+++ backend/src/main/java/com/cbu/backend/studyactivitylog/controller/StudyActivityLogController.java
@@ -1,7 +1,38 @@
 package com.cbu.backend.studyactivitylog.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.cbu.backend.studyactivitylog.dto.request.StudyActivityLogRequestDTO;
+import com.cbu.backend.studyactivitylog.dto.response.StudyActivityLogResponseDTO;
+import com.cbu.backend.studyactivitylog.service.StudyActivityLogService;
+import com.cbu.backend.global.ResponseBody;
+import com.cbu.backend.global.ResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
+@RequestMapping("api/study-group/{id}/journal")
+@RequiredArgsConstructor
 public class StudyActivityLogController {
+
+    private StudyActivityLogService studyActivityLogService;
+
+    @DeleteMapping("{journalId}}")
+    public ResponseEntity<ResponseBody<StudyActivityLogResponseDTO>> deleteStudyActivityLog(
+            @PathVariable Long id,
+            @PathVariable Long journalId
+    ) {
+        StudyActivityLogResponseDTO deleteStudyActivityLog = studyActivityLogService.delete(journalId);
+//        ResponseBody<StudyActivityLogResponseDTO> = new ResponseBody<>();
+//        return ResponseEntity.ok(responseBody);
+    }
+
+//    @GetMapping
+//    public ResponseEntity<?> getAllStudyActivityLog() {
+//
+//    }
+
 }
--- backend/src/main/java/com/cbu/backend/studyactivitylog/service/StudyActivityLogService.java
+++ backend/src/main/java/com/cbu/backend/studyactivitylog/service/StudyActivityLogService.java
@@ -1,11 +1,16 @@
 package com.cbu.backend.studyactivitylog.service;
 
+import com.cbu.backend.studyactivitylog.dto.response.StudyActivityLogResponseDTO;
+import com.cbu.backend.studyactivitylog.entity.StudyActivityLog;
 import com.cbu.backend.studygroup.entity.StudyGroup;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class StudyActivityLogService {
 
     // TODO 덤프 메소드입니다. 가장 최근 스터디 일지의 생성 시간을 반환하도록 추후 구현해주세요.
@@ -14,4 +19,11 @@ public class StudyActivityLogService {
         return null;
     }
 
+
+    @Transactional
+    public StudyActivityLogResponseDTO delete(Long id) {
+        StudyActivityLog studyActivityLog;
+        studyActivityLog.getStudyGroup().delete();
+        return studyActivityLogMapper.toDto(studyActivityLog);
+    }
 }
