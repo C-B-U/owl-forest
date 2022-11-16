7eb6027a349fdfab37b6950da5d5d5c89a1ea41c
suhyeon <suhyeon3484@naver.com>
2022-11-17 00:53:54 +0900

    refactor/#45/디자인을 위한 리펙터링

--- frontend/src/Pages/AL_Pages/StudyCrew.jsx
+++ frontend/src/Pages/AL_Pages/StudyCrew.jsx
@@ -6,6 +6,7 @@ import Logo10 from '../../Image/Logo10.png';
 import Input from '../../Components/Input';
 import Btn from '../../Components/Btn';
 import Plus from '../../Image/PlusButton.png';
+import Minus from '../../Image/MinusButton.png';
 // import Scroll from '../../Components/Scroll';
 
 const HeaderWrapper = styled.div`
@@ -38,8 +39,8 @@ const MainWrap = styled.div`
   display: inline-block;
   background-color: ${palette('PsCocoa', 0)};
   background-size: cover;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   text-align: center;
 `;
 
@@ -52,27 +53,35 @@ const OutLine = styled.div`
   width: 100rem;
   height: 40rem;
   border-radius: 1rem;
-  margin: auto;
+  /* margin: auto; */
   padding: 4rem;
   background-color: ${palette('PsLightBrown')};
   display: inline-block;
   text-align: center;
 `;
 
-// const Study = styled.div`
-//   display: flex;
-// `;
-
-const Member = styled.div`
-  margin: 0.2rem;
-`;
-
+/** 블럭 간격 조절 */
 const StudyIndex = styled.div`
   display: flex;
   margin: 1rem;
   margin-right: 10rem;
-  align-items: center;
+`;
+
+const BtnWrap = styled.div`
+  display: flex;
+  margin-left: 1rem;
+`;
+
+const Team = styled.div`
+  display: flex;
   justify-content: center;
+`;
+
+const WordWrap = styled.div`
+  display: inline-block;
+`;
+const BlockWrap = styled.div`
+  display: inline-block;
 `;
 
 const StudyHeader = styled.header`
@@ -86,11 +95,38 @@ const StudyHeader = styled.header`
   justify-content: left;
   display: flex;
 `;
-
-const Studylist = styled.header`
+/** 스터디 개요 */
+const StudySummary = styled.header`
   width: 10rem;
   height: 4rem;
-  margin-top: 1rem;
+  margin-top: 0.3rem;
+  margin-bottom: 5.8rem;
+  margin-right: 1rem;
+  font-size: 20pt;
+  font-weight: bold;
+  align-items: center;
+  justify-content: right;
+  display: flex;
+`;
+
+/** 팀장 */
+const StudyLeader = styled.header`
+  width: 10rem;
+  height: 4rem;
+  /* margin-top: 1rem; */
+  margin-right: 1rem;
+  font-size: 20pt;
+  font-weight: bold;
+  align-items: center;
+  justify-content: right;
+  display: flex;
+`;
+
+/** 팀원 */
+const StudyMember = styled.header`
+  width: 10rem;
+  height: 4rem;
+  margin-top: 0.1rem;
   margin-right: 1rem;
   font-size: 20pt;
   font-weight: bold;
@@ -101,6 +137,18 @@ const Studylist = styled.header`
 
 const PlusBtn = styled.button`
   background-image: url(${Plus});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 2rem;
+  background-color: ${palette('PsLightBrown')};
+  border: 0em;
+  width: 3rem;
+  height: 3rem;
+  margin-left: 0.5rem;
+`;
+
+const MinusBtn = styled.button`
+  background-image: url(${Minus});
   background-repeat: no-repeat;
   background-position: center;
   background-size: 2rem;
@@ -123,48 +171,79 @@ function StudyCrew() {
           <StudyWrap>
             <StudyHeader>스터디 수정</StudyHeader>
             <OutLine>
-              {/* <Study> */}
-              <StudyIndex>
-                <Studylist>스터디 개요</Studylist>
-                <Input width={'40.5rem'} height={'8rem'} />
-              </StudyIndex>
-              <Member>
-                <StudyIndex>
-                  <Studylist>팀장</Studylist>
-                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
-                  <Input width={'30rem'} height={'3rem'} />
-                </StudyIndex>
-                <StudyIndex>
-                  <Studylist>팀원</Studylist>
-                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
-                  <Input width={'30rem'} height={'3rem'} />
-                  {/* <PlusBtn></PlusBtn> */}
-                </StudyIndex>
-                <StudyIndex>
-                  <Studylist></Studylist>
-                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
-                  <Input width={'30rem'} height={'3rem'} />
-                </StudyIndex>
-                <StudyIndex>
-                  <Studylist></Studylist>
-                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
-                  <Input width={'30rem'} height={'3rem'} />
-                </StudyIndex>
-                <StudyIndex>
-                  <Studylist></Studylist>
-                  <Input width={'7rem'} height={'3rem'} margin={'0 1rem 0 0'} />
-                  <Input width={'30rem'} height={'3rem'} />
-                </StudyIndex>
-              </Member>
-              {/* </Study> */}
-              <PlusBtn></PlusBtn>
-              <Btn
-                background={palette('PsBtn')}
-                color={palette('PsYellow')}
-                width={'50rem'}
-                height={'3.5rem'}
-                name={'수정하기'}
-              />
+              <Team>
+                {/* 글씨 블럭 */}
+                <WordWrap>
+                  <StudySummary>스터디 개요</StudySummary>
+                  <StudyLeader>팀장</StudyLeader>
+                  <StudyMember>팀원</StudyMember>
+                </WordWrap>
+
+                {/* 블럭 */}
+                <BlockWrap>
+                  <StudyIndex>
+                    {/* 스터디 개요 작성 */}
+                    <Input width={'43.5rem'} height={'8rem'} />
+                  </StudyIndex>
+                  <StudyIndex>
+                    {/* 팀장 작성 */}
+                    <Input
+                      width={'7rem'}
+                      height={'3rem'}
+                      margin={'0 1rem 0 0'}
+                    />
+                    <Input width={'30rem'} height={'3rem'} />
+                  </StudyIndex>
+                  <StudyIndex>
+                    {/* 팀원작성 */}
+                    <Input
+                      width={'7rem'}
+                      height={'3rem'}
+                      margin={'0 1rem 0 0'}
+                    />
+                    <Input width={'30rem'} height={'3rem'} />
+                    <PlusBtn></PlusBtn>
+                  </StudyIndex>
+                  <StudyIndex>
+                    <Input
+                      width={'7rem'}
+                      height={'3rem'}
+                      margin={'0 1rem 0 0'}
+                    />
+                    <Input width={'30rem'} height={'3rem'} />
+                    <MinusBtn></MinusBtn>
+                  </StudyIndex>
+                  <StudyIndex>
+                    <Input
+                      width={'7rem'}
+                      height={'3rem'}
+                      margin={'0 1rem 0 0'}
+                    />
+                    <Input width={'30rem'} height={'3rem'} />
+                    <MinusBtn></MinusBtn>
+                  </StudyIndex>
+                  <StudyIndex>
+                    <Input
+                      width={'7rem'}
+                      height={'3rem'}
+                      margin={'0 1rem 0 0'}
+                    />
+                    <Input width={'30rem'} height={'3rem'} />
+                    <MinusBtn></MinusBtn>
+                  </StudyIndex>
+
+                  {/* 버튼 */}
+                  <BtnWrap>
+                    <Btn
+                      background={palette('PsBtn')}
+                      color={palette('PsYellow')}
+                      width={'45.5rem'}
+                      height={'3.5rem'}
+                      name={'수정하기'}
+                    />
+                  </BtnWrap>
+                </BlockWrap>
+              </Team>
             </OutLine>
           </StudyWrap>
         </MainWrap>
