e4b970b7dc8d6d7e94c01baddca3f2146c22d0a7
JongTTing <ejh990521@gmail.com>
2022-11-19 16:47:40 +0900

    북리뷰 respository 구현

--- backend/src/main/java/com/cbu/backend/bookreview/repository/BookReviewRepository.java
+++ backend/src/main/java/com/cbu/backend/bookreview/repository/BookReviewRepository.java
@@ -1,7 +1,22 @@
 package com.cbu.backend.bookreview.repository;
 
+import com.cbu.backend.bookreview.entity.BookReview;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BookReviewRepository {
+public interface BookReviewRepository extends JpaRepository<BookReview, Long> {
+    List<BookReview> findAllByTitleContaining(String title);
+
+    List<BookReview> findAllByOrderByStarDesc();
+
+    List<BookReview> findAllByOrderByLevelDesc();
+
+    @Query("SELECT b FROM BookReview AS b ORDER BY b.baseTime.createdAt desc")
+    List<BookReview> findAllByOrderByCreateAtDesc();
+
 }
