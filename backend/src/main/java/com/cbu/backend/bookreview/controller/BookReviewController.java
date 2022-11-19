261689957305598a4527febad166de4317376d0b
JongTTing <ejh990521@gmail.com>
2022-11-19 16:48:54 +0900

    북리뷰 Controller 구현

--- backend/src/main/java/com/cbu/backend/bookreview/controller/BookReviewController.java
+++ backend/src/main/java/com/cbu/backend/bookreview/controller/BookReviewController.java
@@ -1,43 +1,86 @@
 package com.cbu.backend.bookreview.controller;
 
 import com.cbu.backend.bookreview.dto.request.CreateBookReviewRequest;
+import com.cbu.backend.bookreview.dto.response.BookReviewResponse;
 import com.cbu.backend.bookreview.service.BookReviewService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import com.cbu.backend.global.ResponseBody;
+import com.cbu.backend.global.ResponseStatus;
+import java.util.List;
 
 @RestController
-@RequestMapping("/bookReview")
+@RequestMapping("api/book-review")
 public class BookReviewController {
 
-    //북피뷰 등록
-    @PostMapping
-    public void enrollBookReview(@RequestBody CreateBookReviewRequest createBookReviewRequestDTO) {
-        BookReviewService bookReviewService = new BookReviewService();
-        //bookReviewService.createBookReview(createBookReviewRequestDTO);
+    private BookReviewService bookReviewService;
+
+    public BookReviewController(BookReviewService bookReviewService) {
+        this.bookReviewService = bookReviewService;
     }
 
-    //북리뷰 리스트 조회
-    @GetMapping
-    public void findBookReviewList() {
+    @PostMapping()
+    public ResponseEntity<ResponseBody<BookReviewResponse>> createBookReview(
+            @RequestBody CreateBookReviewRequest createBookReviewRequest) {
+        BookReviewResponse saveBookReview = bookReviewService.save(createBookReviewRequest);
+        ResponseBody<BookReviewResponse> responseBody
+                = new ResponseBody<>(ResponseStatus.POST_BOOKREVIEW_SUCCESS, saveBookReview);
+
+        return ResponseEntity.ok(responseBody);
     }
 
-    //북리뷰 한줄평으로 조회
-    @GetMapping
-    public void findBookReviewByTitle(@RequestParam String simpleReview) {
+    @DeleteMapping("{id}")
+    public ResponseEntity<ResponseBody<BookReviewResponse>> deleteBookReview(@PathVariable Long id) {
+        BookReviewResponse deletedBookReview = bookReviewService.deleteById(id);
+        ResponseBody<BookReviewResponse> responseBody = new ResponseBody<>(ResponseStatus.DELETE_BOOKREVIEW_SUCCESS, deletedBookReview);
+        return ResponseEntity.ok(responseBody);
     }
 
-    //북리뷰 필터 적용하여 조회
-    @GetMapping("/bookReview")
-    public void findBookReviewByFilter(@RequestParam Map<String, String> filter) {
+    @GetMapping()
+    public ResponseEntity<ResponseBody<List<BookReviewResponse>>> getBookReviewListAll() {
+        List<BookReviewResponse> bookReviews = bookReviewService.getAll();
+        ResponseBody<List<BookReviewResponse>> responseBody = new ResponseBody<>(ResponseStatus.GET_BOOKREVIEW_LIST_SUCCESS, bookReviews);
+
+        return ResponseEntity.ok(responseBody);
     }
 
-    //북리뷰 상세조회
-    @GetMapping("/bookReview/{id}")
-    public void findBookReviewDetail(@PathVariable String id) {
+    @GetMapping()
+    public ResponseEntity<ResponseBody<List<BookReviewResponse>>> getBookReviewByTitle(@RequestParam String title) {
+        List<BookReviewResponse> bookReviews = bookReviewService.getAllByTitle(title);
+        ResponseBody<List<BookReviewResponse>> responseBody = new ResponseBody<>(ResponseStatus.GET_BOOKREVIEW_LIST_SUCCESS, bookReviews);
+
+        return ResponseEntity.ok(responseBody);
     }
 
-    @DeleteMapping("/bookReview/{id}")
-    public void deleteBookReview(@PathVariable String id) {
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseBody<BookReviewResponse>> getBookReviewById(@PathVariable Long id) {
+        BookReviewResponse bookReview = bookReviewService.getById(id);
+        ResponseBody<BookReviewResponse> responseBody = new ResponseBody<>(ResponseStatus.GET_BOOKREVIEW_SUCCESS, bookReview);
+
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @GetMapping(params = "sortBy=star")
+    public ResponseEntity<ResponseBody<List<BookReviewResponse>>> getBookReviewSortByStar(@RequestParam String sort) {
+        List<BookReviewResponse> bookReviews = bookReviewService.getOrderByStar(sort);
+        ResponseBody<List<BookReviewResponse>> responseBody = new ResponseBody<>(ResponseStatus.GET_BOOKREVIEW_LIST_SUCCESS, bookReviews);
+
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @GetMapping(params = "sortBy=level")
+    public ResponseEntity<ResponseBody<List<BookReviewResponse>>> getBookReviewSortByLevel(@RequestParam String sort) {
+        List<BookReviewResponse> bookReviews = bookReviewService.getOrderByLevel(sort);
+        ResponseBody<List<BookReviewResponse>> responseBody = new ResponseBody<>(ResponseStatus.GET_BOOKREVIEW_LIST_SUCCESS, bookReviews);
+
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @GetMapping(params = "sortBy=createAt")
+    public ResponseEntity<ResponseBody<List<BookReviewResponse>>> getBookReviewSortByCreateAt(@RequestParam String sort) {
+        List<BookReviewResponse> bookReviews = bookReviewService.getOrderByCreateAt(sort);
+        ResponseBody<List<BookReviewResponse>> responseBody = new ResponseBody<>(ResponseStatus.GET_BOOKREVIEW_LIST_SUCCESS, bookReviews);
+
+        return ResponseEntity.ok(responseBody);
     }
 }
