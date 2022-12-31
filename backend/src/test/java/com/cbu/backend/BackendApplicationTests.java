67a0760874f1a7c2b10b5fce9cbede111306f38d
Hyeonjun Park <phjppo0918@gmail.com>
2022-12-31 11:43:57 +0900

    feat: init setting github actions

--- backend/src/test/java/com/cbu/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/cbu/backend/BackendApplicationTests.java
@@ -2,12 +2,15 @@ package com.cbu.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yml" })
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
