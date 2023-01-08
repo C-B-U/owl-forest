8d8e3429cea18a3fdb70a79979ce17615c75a50b
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-08 20:59:41 +0900

    feat : member 정보 업데이트 dto 구현

---
+++ backend/src/main/java/com/cbu/backend/member/dto/request/UpdateMemberRequest.java
@@ -0,0 +1,34 @@
+package com.cbu.backend.member.dto.request;
+
+import com.cbu.backend.member.entity.AcademicStatus;
+import com.cbu.backend.member.entity.Major;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateMemberRequest {
+    @NotBlank
+    private String nickname;
+    @URL
+    private String blogUrl;
+    private String githubId;
+    @NotBlank
+    private AcademicStatus academicStatus;
+    @NotBlank
+    private Major major;
+    private Major subMajor;
+    @Email
+    private String email;
+    private String introduction;
+    @NotBlank
+    private String studentId;
+    @NotBlank
+    private String phoneNumber;
+}
