9e231714b6b6ca61b865f61282279160b22c87d3
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-10 18:08:27 +0900

    fix: 소셜 로그인 후 redirect 변경

--- backend/src/main/java/com/cbu/backend/config/security/oauth2/OAuth2AuthSuccessHandler.java
+++ backend/src/main/java/com/cbu/backend/config/security/oauth2/OAuth2AuthSuccessHandler.java
@@ -1,18 +1,15 @@
 package com.cbu.backend.config.security.oauth2;
 
 import com.cbu.backend.config.security.jwt.JwtSetupService;
-
-import lombok.RequiredArgsConstructor;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * OAuth2 사용지 인증 성공시 진행하는 Handler
@@ -20,9 +17,15 @@ import javax.servlet.http.HttpServletResponse;
  * @author Hyeonjun Park
  */
 @Component
-@RequiredArgsConstructor
 public class OAuth2AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtSetupService jwtSetupService;
+
+    private final String redirectUrl;
+
+    public OAuth2AuthSuccessHandler(JwtSetupService jwtSetupService, @Value("${client.url}") String redirectUrl) {
+        this.jwtSetupService = jwtSetupService;
+        this.redirectUrl = redirectUrl;
+    }
 
     @Override
     public void onAuthenticationSuccess(
@@ -31,7 +34,7 @@ public class OAuth2AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHand
 
         LoginUser loginUser = getOAuthUser((OAuth2User) authentication.getPrincipal());
         jwtSetupService.addJwtTokensInCookie(response, loginUser);
-        response.sendRedirect("/");
+        response.sendRedirect(redirectUrl);
     }
 
     private LoginUser getOAuthUser(OAuth2User oAuth2User) {
