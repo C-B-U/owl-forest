35c172a9a9b32ab8fbaeaf1884a91234ebd0ecbc
김혜진 <khj000217@naver.com>
2023-01-11 00:39:02 +0900

    feat:팝업 완료, DatePicker 달력 추가

--- frontend/src/Pages/BR/BookReg.jsx
+++ frontend/src/Pages/BR/BookReg.jsx
@@ -1,89 +1,14 @@
 import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
+import { ko } from 'date-fns/esm/locale';
 import { palette } from 'styled-tools';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import theme from '../../Components/Color';
 import Header from '../../Components/ActivityLog/Header';
 import SearchIcon from '../../Image/SearchIcon.png';
 import Button from '../../Components/Btn';
 import Input from '../../Components/Input';
-
-// ------------- 책등록 페이지 -------------
-
-const MainWrap = styled.div`
-  position: absolute;
-  background-color: ${palette('PsCocoa', 0)};
-  width: 100%;
-  height: calc(100% - 4rem);
-`;
-
-const Wrap = styled.div`
-  margin: 4rem auto 0rem auto;
-  width: fit-content;
-  height: fit-content;
-  background-color: ${palette('PsCocoa', 1)};
-  padding: 3rem;
-  box-sizing: content-box;
-`;
-
-const Title = styled.h1`
-  color: ${palette('PsYellow')};
-`;
-
-const WrapContent = styled.div`
-  display: flex;
-`;
-
-const WrapBookImage = styled.div`
-  width: 15rem;
-  height: 19rem;
-  background-color: #ffffff;
-`;
-
-const WrapRegister = styled.div`
-  margin-left: 3rem;
-`;
-const WrapBookTitle = styled.div`
-  /* width: fit-content;
-  height: fit-content;
-  background-color: white;
-  border-radius: 0.2rem; */
-`;
-
-const WrapBookDetail = styled.div`
-  /* width: fit-content;
-  height: fit-content;
-  background-color: white;
-  border-radius: 0.2rem; */
-  margin-top: 1rem;
-`;
-
-const WrapBookReturn = styled.div`
-  width: fit-content;
-  height: 13.3rem;
-  padding: 1rem;
-  background-color: ${palette('PsLightBrown', 0)};
-  /* background-color: white; */
-  border-radius: 0.2rem;
-  margin-top: 2rem;
-  /* float: right; */
-`;
-
-const WrapReturnAlert = styled.div`
-  display: flex;
-`;
-
-const ReturnAlert = styled.div`
-  margin-right: 1rem;
-  margin-bottom: 1rem;
-`;
-
-const DetailDate = styled.div`
-  margin-bottom: 1rem;
-`;
-
-const WrapRegButton = styled.div`
-  float: right;
-`;
 
 // ---------------- 팝업창 작업----------------
 
@@ -106,6 +31,11 @@ const WrapPopup = styled.div`
   border-radius: 1rem;
 `;
 
+const WrapCloseButton = styled.div`
+  margin-top: 1.5rem;
+  margin-left: 2.5rem;
+`;
+
 const ISBNSearch = styled.div`
   font-size: 3rem;
   margin-top: 4rem;
@@ -120,15 +50,7 @@ const WrapSearchbar = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
-
-const SearchForm = styled.form`
-  background-image: url(${SearchIcon});
-  background-position: right;
-  width: 40rem;
-  height: 3.2rem;
-  background-size: 3rem;
-  background-repeat: no-repeat;
+  z-index: 99;
 `;
 
 const WrapSearchIcon = styled.div`
@@ -194,12 +116,120 @@ const ReleaseDate = styled.div`
   text-align: center;
 `;
 
+// ------------- 책등록 페이지 -------------
+
+const MainWrap = styled.div`
+  position: absolute;
+  background-color: ${palette('PsCocoa', 0)};
+  width: 100%;
+  height: calc(100% - 4rem);
+`;
+
+const Wrap = styled.div`
+  margin: 4rem auto 0rem auto;
+  width: fit-content;
+  height: fit-content;
+  background-color: ${palette('PsCocoa', 1)};
+  padding: 3rem;
+  box-sizing: content-box;
+`;
+
+const Title = styled.h1`
+  color: ${palette('PsYellow')};
+`;
+
+const WrapContent = styled.div`
+  display: flex;
+`;
+
+const WrapBookImage = styled.div`
+  width: 15rem;
+  height: 19rem;
+  background-color: #ffffff;
+`;
+
+const WrapBookSearch = styled.div`
+  margin-top: 20rem;
+  margin-left: -15rem;
+`;
+
+const WrapRegister = styled.div`
+  margin-left: 3rem;
+`;
+const WrapBookTitle = styled.div`
+  font-weight: bold;
+  font-size: 1.5rem;
+`;
+
+const WrapBookDetail = styled.div`
+  font-size: 1.3rem;
+  margin-top: 1rem;
+`;
+
+const WrapBookReturn = styled.div`
+  width: fit-content;
+  height: 13.3rem;
+  padding: 1rem;
+  background-color: ${palette('PsLightBrown', 0)};
+  border-radius: 0.2rem;
+  margin-top: 2rem;
+`;
+
+const WrapReturnAlert = styled.div`
+  display: flex;
+`;
+
+const ReturnAlert = styled.div`
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  width: 6rem;
+`;
+
+const DetailDate = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const PickDate = styled(DatePicker)`
+  width: 11rem;
+  height: 1.2rem;
+  box-sizing: border-box;
+  text-align: center;
+  margin-top: -0.5rem;
+  padding: 0.8rem 2rem;
+  border-radius: 0.4rem;
+  border: 1px solid grey;
+  font-size: 0.9rem;
+  z-index: -1;
+`;
+
+const WrapRegButton = styled.div`
+  margin-top: 4.5rem;
+  float: right;
+`;
+
+const Test = styled.div`
+  width: 100vw;
+  height: 200vh;
+  background-color: black;
+`;
+
 function BookReg() {
   // 팝업창 x 버튼 기능 구현
   const [isShown, setIsShown] = useState(false);
 
-  const handleClick = (e) => {
-    setIsShown((current) => !current);
+  // 달력 보이기
+  const [startDate, setStartDate] = useState(new Date());
+
+  // 팝업 열기
+  const openPopup = () => {
+    console.log('open');
+    setIsShown(true);
+  };
+
+  // 팝업 닫기
+  const closePopup = () => {
+    console.log('close');
+    setIsShown(false);
   };
 
   return (
@@ -210,17 +240,25 @@ function BookReg() {
           {/* 
               ---------------- 팝업창 작업----------------
           */}
-
           <WrapPopupBackground style={{ display: isShown ? 'block' : 'none' }}>
             <WrapPopup>
-              <Button onclick={handleClick} name='x' />
-              <ISBNSearch>ISBN 검색</ISBNSearch>
+              <WrapCloseButton>
+                <Button
+                  background='transparent'
+                  name='x'
+                  borderStyle='none'
+                  fontSize='3rem'
+                  onClick={closePopup}
+                />
+              </WrapCloseButton>
+              <ISBNSearch>도서 검색</ISBNSearch>
               <WrapSearchbar>
                 {/* <SearchForm></SearchForm> */}
                 <Input
-                  width='40rem'
+                  width='36rem'
                   height='3.2rem'
-                  placeholder='ISBN을 적어주세요.'
+                  placeholder='도서 제목을 적어주세요.'
+                  fontSize='1.2rem'
                 />
                 <WrapSearchIcon />
               </WrapSearchbar>
@@ -248,33 +286,42 @@ function BookReg() {
           {/*
               ------------- 책등록 페이지 작업 -------------
            */}
-
           <Wrap>
             <Title>도서 등록</Title>
-            {/* <button onclick={handleClick}>팝업 여는 테스트 버튼</button> */}
             <WrapContent>
               <WrapBookImage />
+              <WrapBookSearch>
+                <Button
+                  onClick={openPopup}
+                  color={palette('PsBtn')}
+                  background={palette('PsGreen')}
+                  width='15rem'
+                  height='2.5rem'
+                  name='도서 검색'
+                />
+              </WrapBookSearch>
+
               <WrapRegister>
-                <WrapBookTitle>
-                  <Input width='40rem' height='2.3rem' placeholder='책 제목' />
-                </WrapBookTitle>
-                <WrapBookDetail>
-                  <Input
-                    width='40rem'
-                    height='2.3rem'
-                    placeholder='( 작가 | 출판사 | 출판일 )'
-                  />
-                </WrapBookDetail>
+                <WrapBookTitle>도서 제목</WrapBookTitle>
+                <WrapBookDetail>작가 | 출판사 | 출판일</WrapBookDetail>
                 <WrapBookReturn>
                   <WrapReturnAlert>
                     <ReturnAlert>반납 마감일</ReturnAlert>
-                    <ReturnAlert>반납날짜 : 연도 | 월 | 일 </ReturnAlert>
+
+                    <div>
+                      <PickDate
+                        locale={ko}
+                        dateFormat='yyyy일 MM월 dd일'
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                      />
+                    </div>
                   </WrapReturnAlert>
                   <DetailDate>
                     <Input
-                      width='38rem'
-                      height='7rem'
-                      placeholder='달력..이미지 어떡하죠'
+                      width='25rem'
+                      height='2.8rem'
+                      placeholder='카카오 오픈 채팅 주소'
                     />
                   </DetailDate>
                   <WrapRegButton>
