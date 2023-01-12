1c2faf05cd8b5a19693918b6b3d119208ee4bbaa
Choejeonghun <saa00123@naver.com>
2023-01-12 21:17:37 +0900

    feat/#179/studymanage 페이지 디자인

--- frontend/src/Pages/SAL/StudyManage.jsx
+++ frontend/src/Pages/SAL/StudyManage.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { palette } from 'styled-tools';
 import theme from '../../Components/Color';
-import StudyBtn from '../../Components/Btn';
 import Header from '../../Components/ActivityLog/Header';
-import Profile from '../../Components/ActivityLog/Profile';
-import Input from '../../Components/Input';
+import Btn from '../../Components/Btn';
+import StudyManageProfile from '../../Components/ActivityLog/StudyManageProfile';
 
 const MainWrap = styled.div`
   display: flex;
@@ -13,79 +12,81 @@ const MainWrap = styled.div`
   background-color: ${palette('PsCocoa', 0)};
 `;
 
-const ContextWrap = styled.div`
-  background-color: ${palette('PsLightBrown')};
-  border-radius: 10px;
-  padding: 3rem 2rem;
-
-  box-sizing: border-box;
-
-  display: flex;
-  flex-direction: column;
-
-  align-items: flex-end;
-`;
-
-const HeaderTitle = styled.header`
+const HeaderWrap = styled.header`
   height: 4rem;
   padding-left: 1rem;
-  font-size: 20pt;
-  font-weight: bold;
   color: #fafad2;
   align-items: center;
   justify-content: left;
   display: flex;
 `;
 
-const RightMainWrap = styled.div`
-  margin: 3rem 0px;
-  width: 100%;
-
-  margin-right: 5rem;
+const HeaderTitle = styled.header`
+  font-size: 20pt;
+  font-weight: bold;
+  color: #fafad2;
+  align-items: center;
+  justify-content: left;
 `;
 
-const BtnWrap = styled.div`
-  width: calc(100% - 7rem);
-  margin-top: 0.5rem;
+const BtnWrap = styled.header`
+  font-size: 20pt;
+  font-weight: bold;
+  color: #fafad2;
+  align-items: center;
+  display: flex;
   margin-left: auto;
 `;
-const InputTitle = styled.div`
-  font-size: 20px;
-  font-weight: 500;
-  margin-top: 0.8rem;
-  margin-right: 1rem;
 
-  width: 6rem;
-  text-align: end;
-`;
-const InputWrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: flex-end;
-
-  margin: 0.8rem 0;
-`;
-const Center = styled.div`
-  margin: 0 auto;
-`;
-
-const InputMemberWrapper = styled.div`
-  width: 636.8px;
-
-  display: flex;
-
-  & > div:nth-child(1) {
-    margin-right: 0.4rem;
+const BtnItems = styled.div`
+  font-size: 15pt;
+  margin: 1rem;
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-underline-position: under;
+    text-decoration-thickness: 0.2rem;
+    text-decoration-color: ${palette('PsPurple', 0)};
   }
 `;
 
-const ScorllWrapper = styled.div`
+const RightMainWrap = styled.div`
+  margin: 5rem 0px;
   width: 100%;
-  height: 270px;
+  margin-right: 5rem;
+`;
 
+const StudyList = styled.div`
+  width: 71rem;
+  height: 8rem;
+  border-radius: 1rem;
+  margin-top: 0.8rem;
+  padding: 1.5rem;
+  display: flex;
+  background-color: ${palette('PsLightBrown')};
+`;
+
+const StudyName = styled.div`
+  font-size: 15pt;
+  font-weight: bold;
+  align-items: center;
+  justify-content: left;
+  display: flex;
+  padding: 1rem;
+  margin: 1rem;
+`;
+
+const StudyInput = styled.div`
+  font-size: 10pt;
+  font-weight: bold;
+  display: flex;
+`;
+
+const Scroll = styled.div`
+  width: 100%;
+  height: 43.5rem;
   margin-right: -7px;
   overflow-y: auto;
-
   &::-webkit-scrollbar {
     background: white;
     width: 7px;
@@ -98,99 +99,68 @@ const ScorllWrapper = styled.div`
     margin-top: 0px;
   }
 `;
-const InputIcon = styled.div`
-  font-size: 36px;
-  font-weight: bold;
 
-  display: flex;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-
-  padding-bottom: 0.5rem;
-
-  color: ${(props) => (props.color ? props.color : '#000000')};
-`;
 function StudyManage() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
       <MainWrap>
-        <Profile />
+        <StudyManageProfile />
         <RightMainWrap>
-          <HeaderTitle>스터디 생성</HeaderTitle>
-          <ContextWrap>
-            <Center>
-              <InputWrapper>
-                <InputTitle>스터디 이름</InputTitle>
-                <Input width='600px' height='50px' placeholder='스터디 이름' />
-              </InputWrapper>
-              <InputWrapper>
-                <InputTitle>스터디 개요</InputTitle>
-                <Input width='600px' height='200px' placeholder='스터디 개요' />
-              </InputWrapper>
-
-              <ScorllWrapper>
-                <InputWrapper>
-                  <InputTitle>팀장</InputTitle>
-                  <InputMemberWrapper>
-                    <Input width='130px' height='50px' placeholder='이름' />
-                    <Input width='360px' height='50px' placeholder='학번' />
-                  </InputMemberWrapper>
-                </InputWrapper>
-                <InputWrapper>
-                  <InputTitle>팀원</InputTitle>
-                  <InputMemberWrapper>
-                    <Input width='130px' height='50px' placeholder='이름' />
-                    <Input width='360px' height='50px' placeholder='학번' />
-                    <InputIcon>+</InputIcon>
-                  </InputMemberWrapper>
-                </InputWrapper>
-                <InputWrapper>
-                  <InputTitle>팀원</InputTitle>
-                  <InputMemberWrapper>
-                    <Input width='130px' height='50px' placeholder='이름' />
-                    <Input width='360px' height='50px' placeholder='학번' />
-                    <InputIcon color='red'>-</InputIcon>
-                  </InputMemberWrapper>
-                </InputWrapper>
-                <InputWrapper>
-                  <InputTitle>팀원</InputTitle>
-                  <InputMemberWrapper>
-                    <Input width='130px' height='50px' placeholder='이름' />
-                    <Input width='360px' height='50px' placeholder='학번' />
-                    <InputIcon color='red'>-</InputIcon>
-                  </InputMemberWrapper>
-                </InputWrapper>
-                <InputWrapper>
-                  <InputTitle>팀원</InputTitle>
-                  <InputMemberWrapper>
-                    <Input width='130px' height='50px' />
-                    <Input width='360px' height='50px' />
-                    <InputIcon color='red'>-</InputIcon>
-                  </InputMemberWrapper>
-                </InputWrapper>
-                <InputWrapper>
-                  <InputTitle>팀원</InputTitle>
-                  <InputMemberWrapper>
-                    <Input width='130px' height='50px' />
-                    <Input width='360px' height='50px' />
-                    <InputIcon color='red'>-</InputIcon>
-                  </InputMemberWrapper>
-                </InputWrapper>
-              </ScorllWrapper>
-
-              <BtnWrap>
-                <StudyBtn
+          <HeaderWrap>
+            <HeaderTitle>활동 일지</HeaderTitle>
+            <BtnWrap>
+              <BtnItems>
+                <Btn
                   background={palette('PsBtn')}
                   color={palette('PsYellow')}
-                  width='100%'
-                  height='3.5rem'
-                  name='생성하기'
+                  width='7rem'
+                  height='3rem'
+                  name='일지 수정'
                 />
-              </BtnWrap>
-            </Center>
-          </ContextWrap>
+              </BtnItems>
+              <BtnItems>
+                <Btn
+                  background={palette('PsBtn')}
+                  color={palette('PsYellow')}
+                  width='7rem'
+                  height='3rem'
+                  name='일지 삭제'
+                />
+              </BtnItems>
+              <BtnItems>
+                <Btn
+                  background={palette('PsBtn')}
+                  color={palette('PsYellow')}
+                  width='7rem'
+                  height='3rem'
+                  name='일지 생성'
+                />
+              </BtnItems>
+            </BtnWrap>
+          </HeaderWrap>
+          <Scroll>
+            <StudyList>
+              <StudyName>1차</StudyName>
+              <StudyInput>
+                날짜 : 2022-9-27
+                <br />
+                <br />
+                시간 : 00:00 ~ 12:00
+                <br />
+                <br />
+                활동 장소 : 비대면 zoom
+                <br />
+                <br />
+                참여 인원 : 가나, 나다, 다라, 마바
+              </StudyInput>
+            </StudyList>
+            <StudyList />
+            <StudyList />
+            <StudyList />
+            <StudyList />
+            <StudyList />
+          </Scroll>
         </RightMainWrap>
       </MainWrap>
     </ThemeProvider>
