1fe7feb23209c4c4441b715126b76003960a2770
Choejeonghun <saa00123@naver.com>
2023-01-17 21:56:34 +0900

    feat: 배경 이미지 추가 및 타이틀 폰트 고민해야함

--- frontend/src/Pages/Welcome.jsx
+++ frontend/src/Pages/Welcome.jsx
@@ -5,14 +5,30 @@ import { palette } from 'styled-tools';
 import theme from '../Components/Color';
 import Btn from '../Components/Btn';
 import CbuIcon from '../Image/CbuIcon.png';
+import Forest from '../Image/forest.png';
 
 const MainWrap = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${Forest});
+  background-size: 100%;
+`;
+
+const Title = styled.div`
+  width: 100vw;
+  height: 15rem;
+  display: flex;
   justify-content: center;
   align-items: center;
-  margin: auto;
+  font-size: 50pt;
+`;
+
+const WelcomeWrap = styled.div`
+  display: flex;
+  margin-top: 7rem;
 `;
 
 const WelcomeImage = styled.div`
@@ -40,33 +56,36 @@ function Welcome() {
     <div>
       <ThemeProvider theme={theme}>
         <MainWrap>
-          <Btn
-            background={palette('PsBtn')}
-            color={palette('PsYellow')}
-            width='20rem'
-            height='20rem'
-            borderRadius='100%'
-            borderStyle='none'
-            fontSize='20pt'
-            onClick={goReg}
-            transform='scale(1.5)'
-            transition='0.5s'
-            name='회원가입'
-          />
-          <WelcomeImage />
-          <Btn
-            background={palette('PsBtn')}
-            color={palette('PsYellow')}
-            width='20rem'
-            height='20rem'
-            borderRadius='100%'
-            borderStyle='none'
-            fontSize='20pt'
-            onClick={goLogin}
-            transform='scale(1.5)'
-            transition='0.5s'
-            name='로그인'
-          />
+          <Title>OWL FOREST</Title>
+          <WelcomeWrap>
+            <Btn
+              background={palette('PsBtn')}
+              color={palette('PsYellow')}
+              width='20rem'
+              height='20rem'
+              borderRadius='100%'
+              borderStyle='none'
+              fontSize='20pt'
+              onClick={goReg}
+              transform='scale(1.5)'
+              transition='0.5s'
+              name='회원가입'
+            />
+            <WelcomeImage />
+            <Btn
+              background={palette('PsBtn')}
+              color={palette('PsYellow')}
+              width='20rem'
+              height='20rem'
+              borderRadius='100%'
+              borderStyle='none'
+              fontSize='20pt'
+              onClick={goLogin}
+              transform='scale(1.5)'
+              transition='0.5s'
+              name='로그인'
+            />
+          </WelcomeWrap>
         </MainWrap>
       </ThemeProvider>
     </div>
