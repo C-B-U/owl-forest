e44e7f1a5d79cbe707a4acd7b8a7f07789b7d3af
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-17 20:39:58 +0900

    test : contextLoads 삭제

--- backend/src/test/java/com/cbu/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/cbu/backend/BackendApplicationTests.java
@@ -1,6 +1,5 @@
 package com.cbu.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -8,6 +7,6 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {}
+   // @Test
+   // void contextLoads() {}
 }
