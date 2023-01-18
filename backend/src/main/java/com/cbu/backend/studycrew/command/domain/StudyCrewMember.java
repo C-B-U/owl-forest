de9ca0a929fac22505ba2b76f2d78788d830358f
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-19 05:07:09 +0900

    fix : 도메인 클래스에 검증 로직 제거
    valication은 controller/service에서 진행해야

--- backend/src/main/java/com/cbu/backend/studycrew/command/domain/StudyCrewMember.java
+++ backend/src/main/java/com/cbu/backend/studycrew/command/domain/StudyCrewMember.java
@@ -1,18 +1,16 @@
 package com.cbu.backend.studycrew.command.domain;
 
 import com.cbu.backend.authaccount.command.domain.AccountNo;
-
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,24 +25,8 @@ public class StudyCrewMember {
     private Set<AccountNo> studyCrewParticipantIds = new HashSet<>();
 
     public StudyCrewMember(AccountNo studyCrewLeaderId, List<AccountNo> studyCrewParticipantIds) {
-        checkLeaderInParticipants(studyCrewLeaderId, studyCrewParticipantIds);
         checkParticipantNotDuplicated(studyCrewParticipantIds);
         this.studyCrewLeaderId = studyCrewLeaderId;
         this.studyCrewParticipantIds.addAll(studyCrewParticipantIds);
-    }
-
-    private void checkParticipantNotDuplicated(List<AccountNo> studyCrewParticipants) {
-        if (studyCrewParticipants.size()
-                != studyCrewParticipants.stream().distinct().mapToInt(o -> 1).sum()) {
-            throw new RuntimeException("중복 멤버가 있습니다.");
-        }
-    }
-
-    private void checkLeaderInParticipants(
-            AccountNo studyCrewLeaderId, List<AccountNo> studyCrewParticipantIds) {
-        if (studyCrewParticipantIds.stream()
-                .anyMatch(participantId -> participantId.equals(studyCrewLeaderId))) {
-            throw new RuntimeException("멤버 리스트에 리더가 중복됩니다.");
-        }
     }
 }
