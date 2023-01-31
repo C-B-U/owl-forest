a7ab126b0ffb1388e26e59f2c718ae3eea725705
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-31 17:06:15 +0900

    fix : cookie 도메인 변경

--- backend/src/main/java/com/cbu/backend/config/security/jwt/JwtSetupService.java
+++ backend/src/main/java/com/cbu/backend/config/security/jwt/JwtSetupService.java
@@ -1,57 +1,51 @@
 package com.cbu.backend.config.security.jwt;
 
 import com.cbu.backend.config.security.oauth2.LoginUser;
-
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * Client의 cookie로 token을 설정
- *
  * @author Hyeonjun Park
  */
-@Slf4j
 @Service
 public class JwtSetupService {
     private final JwtProvider jwtProvider;
-
-    private final String accessTokenHeaderTag;
-    private final String refreshTokenHeaderTag;
+    @Value("${client.host}")
+    private String clientHost;
+    @Value("${jwt.access-header}")
+    private String accessTokenHeaderTag;
+    @Value("${jwt.refresh-header}")
+    private String refreshTokenHeaderTag;
 
     public JwtSetupService(
-            JwtProvider jwtProvider,
-            @Value("${jwt.access-header}") String accessTokenHeaderTag,
-            @Value("${jwt.refresh-header}") String refreshTokenHeaderTag) {
+            JwtProvider jwtProvider) {
         this.jwtProvider = jwtProvider;
-        this.accessTokenHeaderTag = accessTokenHeaderTag;
-        this.refreshTokenHeaderTag = refreshTokenHeaderTag;
     }
 
     /**
      * 인증 login 값을 통해 token을 생성한 다음 클라이언트 쿠키에 저장
-     *
      * @param response
      * @param loginUser
      * @author Hyeonjun Park
      */
     public void addJwtTokensInCookie(HttpServletResponse response, LoginUser loginUser) {
         JwtToken jwtToken = jwtProvider.createJWTTokens(loginUser);
-        Cookie accessTokenCookie = setCookie(accessTokenHeaderTag, jwtToken.getAccessToken());
-        response.addCookie(accessTokenCookie);
-
-        Cookie refreshTokenCookie = setCookie(refreshTokenHeaderTag, jwtToken.getRefreshToken());
-        response.addCookie(refreshTokenCookie);
+        ResponseCookie accessTokenCookie = setCookie(accessTokenHeaderTag, jwtToken.getAccessToken());
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+        ResponseCookie refreshTokenCookie = setCookie(refreshTokenHeaderTag, jwtToken.getRefreshToken());
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
     }
 
-    private Cookie setCookie(String key, String value) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        return cookie;
+    private ResponseCookie setCookie(String key, String value) {
+        return ResponseCookie.from(key, value)
+                .path("/")
+                .httpOnly(true)
+                .sameSite("None")
+                .domain(clientHost)
+                .build();
     }
 }
