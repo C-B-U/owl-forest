442b07c50827a46dc3ca0b4614e219170a478031
Hyeonjun Park <phjppo0918@gmail.com>
2023-02-03 11:51:44 +0900

    hoxfix: 외부 책 조회 api 수정

--- backend/src/main/java/com/cbu/backend/externalbookfinder/query/api/ExternalBookFinderController.java
+++ backend/src/main/java/com/cbu/backend/externalbookfinder/query/api/ExternalBookFinderController.java
@@ -3,22 +3,19 @@ package com.cbu.backend.externalbookfinder.query.api;
 import com.cbu.backend.externalbookfinder.query.dto.BookFinderRequest;
 import com.cbu.backend.externalbookfinder.query.dto.ExternalBookResponse;
 import com.cbu.backend.externalbookfinder.query.service.BookSearchable;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/externalbooks")
+@RequestMapping("externalbooks")
 public class ExternalBookFinderController {
     private final BookSearchable searchBookService;
 
