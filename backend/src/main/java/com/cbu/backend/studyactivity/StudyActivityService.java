8555f1abeae25d786297a590da393a575630fcae
ohksj77 <ohksj77@gmail.com>
2023-02-08 12:00:48 +0900

    chore: delete non-use exception classes

--- backend/src/main/java/com/cbu/backend/studyactivity/StudyActivityService.java
+++ backend/src/main/java/com/cbu/backend/studyactivity/StudyActivityService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
