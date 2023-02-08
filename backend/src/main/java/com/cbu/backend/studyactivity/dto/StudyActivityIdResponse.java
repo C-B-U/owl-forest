00b022a5fc74bf13c9bd1a1788cc6851969021f9
ohksj77 <ohksj77@gmail.com>
2023-02-08 18:12:07 +0900

    feat: study activity api test code

--- backend/src/main/java/com/cbu/backend/studyactivity/dto/StudyActivityIdResponse.java
+++ backend/src/main/java/com/cbu/backend/studyactivity/dto/StudyActivityIdResponse.java
@@ -1,7 +1,9 @@
 package com.cbu.backend.studyactivity.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class StudyActivityIdResponse {
     private Long id;
